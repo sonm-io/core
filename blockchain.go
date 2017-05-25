c660eed66682f9f5663201e704da14fac1228d3b
JackBekket <jackatwork@yandex.ru>
2017-05-26 02:29:45 +0300

    first commit

---
+++ blockchain.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+  "./go-build/SDT.go"
+)
+
+func main() {
+	// Create an IPC based RPC connection to a remote node
+	conn, err := ethclient.Dial("/home/jack/.rinkeby/geth.ipc")
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+	// Instantiate the contract and display its name
+	token, err := NewToken(common.HexToAddress("0x31ac2908b1f981519b7ab992b46eaa41566b3c0a"), conn)
+	if err != nil {
+		log.Fatalf("Failed to instantiate a Token contract: %v", err)
+	}
+	name, err := token.Name(nil)
+	if err != nil {
+		log.Fatalf("Failed to retrieve token name: %v", err)
+	}
+	fmt.Println("Token name:", name)
+}
