1173dc0f81e18b40002a1b53bdc787dac48b1e5f
JackBekket <jackatwork@yandex.ru>
2017-05-26 02:46:37 +0300

    SDT import

--- blockchain.go
+++ blockchain.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-  "github.com/sonm-io/blockchain-api/go-build/"
+  "github.com/sonm-io/blockchain-api/go-build/SDT.go"
 )
 
 func main() {
