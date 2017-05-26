2597619ba55508d839d3027bc4245324e8337b9e
JackBekket <jackatwork@yandex.ru>
2017-05-26 03:13:01 +0300

    name fix

--- go-build/SDT/SDT.go
+++ go-build/SDT/SDT.go
@@ -1,7 +1,7 @@
 // This file is an automatically generated Go binding. Do not modify as any
 // change will likely be lost upon the next re-generation!
 
-package token
+package Token
 
 import (
 	"math/big"
