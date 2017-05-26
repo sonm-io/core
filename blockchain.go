4c50573eab1a22fed6293230175e61ffe98de0e2
JackBekket <jackatwork@yandex.ru>
2017-05-26 03:58:47 +0300

    transfer struct

--- blockchain.go
+++ blockchain.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/big"
+	"strings"
 
 	"github.com/sonm-io/go-ethereum/common"
 	"github.com/sonm-io/go-ethereum/ethclient"
   "github.com/sonm-io/blockchain-api/go-build/SDT"
+
+	"github.com/sonm-io/go-ethereum/accounts/abi/bind"
 )
+
+	const key = `paste the contents of your *testnet* key json here`
 
 func main() {
 	// Create an IPC based RPC connection to a remote node
@@ -25,4 +31,17 @@ func main() {
 		log.Fatalf("Failed to retrieve token name: %v", err)
 	}
 	fmt.Println("Token name:", name)
+
+	// Create an authorized transactor and spend 1 unicorn
+	auth, err := bind.NewTransactor(strings.NewReader(key), "my awesome super secret password")
+	if err != nil {
+		log.Fatalf("Failed to create authorized transactor: %v", err)
+	}
+	tx, err := Token.Transfer(auth, common.HexToAddress("0x0000000000000000000000000000000000000000"), big.NewInt(1))
+	if err != nil {
+		log.Fatalf("Failed to request token transfer: %v", err)
+	}
+	fmt.Printf("Transfer pending: 0x%x\n", tx.Hash())
+
+
 }
