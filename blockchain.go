41334a9ec82dbec08f871013622857910db30bb2
JackBekket <jackatwork@yandex.ru>
2017-05-26 03:15:37 +0300

    brackets

--- blockchain.go
+++ blockchain.go
@@ -16,7 +16,7 @@ func main() {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
 	// Instantiate the contract and display its name
-	token, err := NewToken(common.HexToAddress("0x31ac2908b1f981519b7ab992b46eaa41566b3c0a"), conn)
+	token, err := New(Token(common.HexToAddress("0x31ac2908b1f981519b7ab992b46eaa41566b3c0a"), conn))
 	if err != nil {
 		log.Fatalf("Failed to instantiate a Token contract: %v", err)
 	}
