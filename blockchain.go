9d19b9a8c2bc90d813032a9aac4c6b707808c47f
JackBekket <jackatwork@yandex.ru>
2017-05-27 04:31:36 +0300

    Token transfer

--- blockchain.go
+++ blockchain.go
@@ -3,18 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
-	//"math/big"
-	//"strings"
+	"math/big"
+	"strings"
 	//"bufio"
 
 	"github.com/sonm-io/go-ethereum/common"
 	"github.com/sonm-io/go-ethereum/ethclient"
   "github.com/sonm-io/blockchain-api/go-build/SDT"
 
-	//"github.com/sonm-io/go-ethereum/accounts/abi/bind"
+	"github.com/sonm-io/go-ethereum/accounts/abi/bind"
 )
-
-	const key = `paste the contents of your *testnet* key json here`
+	// THIS IS HACK AND SHOULD BE REWRITTEN
+	const key = `{"address":"fe36b232d4839fae8751fa10768126ee17a156c1","crypto":{"cipher":"aes-128-ctr","ciphertext":"b2f1390ba44929e2144a44b5f0bdcecb06060b5ef1e9b0d222ed0cd5340e2876","cipherparams":{"iv":"a33a90fc4d7a052db58be24bbfdc21a3"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"422328336107aeb54b4a152f4fae0d5f2fbca052fc7688d9516cd998cf790021"},"mac":"08f3fa22882b932ae2926f6bf5b1df2c0795720bd993b50d652cee189c00315c"},"id":"b36be1bf-6eb4-402e-8e26-86da65ae3156","version":3}`
 
 func main() {
 	// Create an IPC based RPC connection to a remote node
@@ -33,17 +33,18 @@ func main() {
 	}
 	fmt.Println("Token name:", name)
 
-/*
+
 	// Create an authorized transactor and spend 1 unicorn
-	auth, err := bind.NewTransactor(strings.NewReader(key), "my awesome super secret password")
+	// yes, this is hack too, need to rewrite it.
+	auth, err := bind.NewTransactor(strings.NewReader(key), "Metamorph9")
 	if err != nil {
 		log.Fatalf("Failed to create authorized transactor: %v", err)
 	}
-	tx, err := Token.Transfer(auth, common.HexToAddress("0x0000000000000000000000000000000000000000"), big.NewInt(1))
+	tx, err := token.Transfer(auth, common.HexToAddress("0x0000000000000000000000000000000000000000"), big.NewInt(1))
 	if err != nil {
 		log.Fatalf("Failed to request token transfer: %v", err)
 	}
 	fmt.Printf("Transfer pending: 0x%x\n", tx.Hash())
-*/
+
 
 }
