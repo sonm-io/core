01f34e745ce7bffe8569989b5b4047653af7ecd2
Anton Tiurin <noxiouz@yandex.ru>
2017-06-09 14:30:29 +0300

    hub: lots of stuff
    * external gRPC API server
    * hanling connections from Miners

--- cmd/hub/main.go
+++ cmd/hub/main.go
@@ -1,130 +1,31 @@
 package main
 
 import (
-	"log"
-	"net"
-	"sync"
-	"time"
-
-	pb "github.com/sonm-io/insonmnia/proto/hub"
-	pbminer "github.com/sonm-io/insonmnia/proto/miner"
+	"os"
+	"os/signal"
 
 	"golang.org/x/net/context"
-	"google.golang.org/grpc"
+
+	"github.com/noxiouz/zapctx/ctxlog"
+	"github.com/sonm-io/insonmnia/insonmnia/hub"
+	"go.uber.org/zap"
 )
-
-const (
-	// TODO: make it configurable
-	externalGRPCEndpoint         = ":10001"
-	minerHubInterconnectEndpoint = ":10002"
-)
-
-type daemon struct {
-	externalGrpc  *grpc.Server
-	minerListener net.Listener
-
-	mu     sync.Mutex
-	miners map[string]pbminer.MinerClient
-
-	wg sync.WaitGroup
-}
-
-func createDaemon() (*daemon, error) {
-	// TODO: add secure mechanism
-	grpcServer := grpc.NewServer()
-	// TODO: assign protocol
-
-	il, err := net.Listen("tcp", minerHubInterconnectEndpoint)
-	if err != nil {
-		return nil, err
-	}
-
-	grpcL, err := net.Listen("tcp", externalGRPCEndpoint)
-	if err != nil {
-		il.Close()
-		return nil, err
-	}
-
-	pb.RegisterHubServer(grpcServer, nil)
-
-	d := &daemon{
-		externalGrpc:  grpcServer,
-		minerListener: il,
-
-		miners: make(map[string]pbminer.MinerClient),
-	}
-
-	d.wg.Add(1)
-	go func() {
-		defer d.wg.Done()
-		d.externalGrpc.Serve(grpcL)
-	}()
-
-	d.wg.Add(1)
-	go func() {
-		defer d.wg.Done()
-		for {
-			conn, err := d.minerListener.Accept()
-			if err != nil {
-				return
-			}
-			go d.handlerInterconnect(conn)
-		}
-	}()
-
-	return d, nil
-}
-
-func (d *daemon) handlerInterconnect(conn net.Conn) {
-	defer conn.Close()
-	log.Printf("miner connected from %v", conn.RemoteAddr())
-
-	cc, err := grpc.Dial("miner", grpc.WithDialer(func(_ string, _ time.Duration) (net.Conn, error) {
-		return conn, nil
-	}))
-	if err != nil {
-		log.Printf("failed to connect to Miner's grpc server: %v", err)
-		return
-	}
-	defer cc.Close()
-	minerClient := pbminer.NewMinerClient(cc)
-
-	d.mu.Lock()
-	d.miners[conn.RemoteAddr().String()] = minerClient
-	d.mu.Unlock()
-
-	defer func() {
-		d.mu.Lock()
-		delete(d.miners, conn.RemoteAddr().String())
-		d.mu.Unlock()
-	}()
-
-	t := time.NewTicker(time.Second * 10)
-	defer t.Stop()
-	for range t.C {
-		// TODO: identify miner via Authorization mechanism
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-		_, err = minerClient.Ping(ctx, &pbminer.PingRequest{})
-		cancel()
-		if err != nil {
-			log.Printf("failed to ping miner %v", err)
-			return
-		}
-	}
-}
-
-func (d *daemon) Stop() {
-	d.externalGrpc.Stop()
-	d.minerListener.Close()
-	d.wg.Wait()
-}
 
 func main() {
-	d, err := createDaemon()
+	ctx := context.Background()
+	h, err := hub.New(ctx)
 	if err != nil {
-		log.Fatalf("failed to create hub daemon: %v", err)
+		ctxlog.GetLogger(ctx).Fatal("failed to create a new Hub", zap.Error(err))
 	}
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		<-c
+		h.Close()
+	}()
 
-	// TODO: call it from SIGNAL handler
-	d.Stop()
+	// TODO: check error type
+	if err = h.Serve(); err != nil {
+		ctxlog.GetLogger(ctx).Error("Server stop", zap.Error(err))
+	}
 }
---
+++ insonmnia/hub/server.go
@@ -0,0 +1,156 @@
+package hub
+
+import (
+	"net"
+	"sync"
+	"time"
+
+	"go.uber.org/zap"
+
+	log "github.com/noxiouz/zapctx/ctxlog"
+	pb "github.com/sonm-io/insonmnia/proto/hub"
+	pbminer "github.com/sonm-io/insonmnia/proto/miner"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	// TODO: make it configurable
+	externalGRPCEndpoint         = ":10001"
+	minerHubInterconnectEndpoint = ":10002"
+)
+
+// Hub collects miners, send them orders to spawn containers, etc.
+type Hub struct {
+	ctx           context.Context
+	externalGrpc  *grpc.Server
+	minerListener net.Listener
+
+	mu     sync.Mutex
+	miners map[string]pbminer.MinerClient
+
+	wg sync.WaitGroup
+}
+
+// Ping should be used as Healthcheck for Hub
+func (h *Hub) Ping(ctx context.Context, _ *pb.PingRequest) (*pb.PingReply, error) {
+	log.G(ctx).Info("reply to Ping")
+	return &pb.PingReply{}, nil
+}
+
+// List returns attached miners
+func (h *Hub) List(context.Context, *pb.ListRequest) (*pb.ListReply, error) {
+	var lr pb.ListReply
+	h.mu.Lock()
+	for k := range h.miners {
+		lr.Name = append(lr.Name, k)
+	}
+	h.mu.Unlock()
+	return &lr, nil
+}
+
+// New returns new Hub
+func New(ctx context.Context) (*Hub, error) {
+	// TODO: add secure mechanism
+	grpcServer := grpc.NewServer()
+	h := &Hub{
+		ctx:          ctx,
+		externalGrpc: grpcServer,
+
+		miners: make(map[string]pbminer.MinerClient),
+	}
+	pb.RegisterHubServer(grpcServer, h)
+
+	return h, nil
+}
+
+// Serve starts handling incoming API gRCP request and communicates
+// with miners
+func (h *Hub) Serve() error {
+	il, err := net.Listen("tcp", minerHubInterconnectEndpoint)
+	if err != nil {
+		log.G(h.ctx).Error("failed to listen", zap.String("address", minerHubInterconnectEndpoint), zap.Error(err))
+		return err
+	}
+	log.G(h.ctx).Info("listening for connections from Miners", zap.Stringer("address", il.Addr()))
+
+	grpcL, err := net.Listen("tcp", externalGRPCEndpoint)
+	if err != nil {
+		log.G(h.ctx).Error("failed to listen",
+			zap.String("address", externalGRPCEndpoint), zap.Error(err))
+		il.Close()
+		return err
+	}
+	log.G(h.ctx).Info("listening for gRPC API conenctions", zap.Stringer("address", grpcL.Addr()))
+	// TODO: fix this possible race
+	h.minerListener = il
+
+	h.wg.Add(1)
+	go func() {
+		defer h.wg.Done()
+		h.externalGrpc.Serve(grpcL)
+	}()
+
+	h.wg.Add(1)
+	go func() {
+		defer h.wg.Done()
+		for {
+			conn, err := h.minerListener.Accept()
+			if err != nil {
+				return
+			}
+			go h.handlerInterconnect(h.ctx, conn)
+		}
+	}()
+	h.wg.Wait()
+
+	return nil
+}
+
+func (h *Hub) handlerInterconnect(ctx context.Context, conn net.Conn) {
+	defer conn.Close()
+	log.G(ctx).Info("miner connected", zap.Stringer("remote", conn.RemoteAddr()))
+
+	cc, err := grpc.Dial("miner", grpc.WithDialer(func(_ string, _ time.Duration) (net.Conn, error) {
+		return conn, nil
+	}))
+	if err != nil {
+		log.G(ctx).Error("failed to connect to Miner's grpc server", zap.Error(err))
+		return
+	}
+	defer cc.Close()
+	minerClient := pbminer.NewMinerClient(cc)
+
+	h.mu.Lock()
+	h.miners[conn.RemoteAddr().String()] = minerClient
+	h.mu.Unlock()
+
+	defer func() {
+		h.mu.Lock()
+		delete(h.miners, conn.RemoteAddr().String())
+		h.mu.Unlock()
+	}()
+
+	t := time.NewTicker(time.Second * 10)
+	defer t.Stop()
+	for range t.C {
+		log.G(ctx).Debug("ping the Miner", zap.Stringer("remote", conn.RemoteAddr()))
+		// TODO: identify miner via Authorization mechanism
+		// TODO: implement retries
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		_, err = minerClient.Ping(ctx, &pbminer.PingRequest{})
+		cancel()
+		if err != nil {
+			log.G(ctx).Error("failed to ping miner", zap.Error(err))
+			return
+		}
+	}
+}
+
+// Close disposes all resources attached to the Hub
+func (h *Hub) Close() {
+	h.externalGrpc.Stop()
+	h.minerListener.Close()
+	h.wg.Wait()
+}
