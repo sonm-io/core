9a8f2cfa095ce3b3467e14292a6631387e875bb2
Anton Shchukin <szwtdy@gmail.com>
2017-06-12 08:13:49 +0500

    add needle utils

---
+++ util.go
@@ -0,0 +1,31 @@
+package Fusrodah
+
+import(
+	"github.com/sonm-io/go-ethereum/crypto"
+	"crypto/ecdsa"
+	"net"
+)
+
+func ToPubKey(prv *ecdsa.PrivateKey) *ecdsa.PublicKey{
+	pkBytes := crypto.FromECDSA(prv)
+	pk := crypto.ToECDSAPub(pkBytes)
+	return pk
+}
+
+
+
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
