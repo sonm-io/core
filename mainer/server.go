c33b6c1c3c2823f09ba89317d10e514ca7e896a6
Antony Shchukin <szwtdy@gmail.com>
2017-07-19 19:17:21 +0300

    Add proto for Hub/Miner Usage

---
+++ mainer/server.go
@@ -0,0 +1,29 @@
+package mainer
+
+import "github.com/sonm-io/Fusrodah"
+
+type Server struct {
+	frd *Fusrodah.Fusrodah
+	ip  string
+}
+
+func NewServer() Server {
+	srv := Server{}
+	return srv
+}
+
+func init() {
+
+}
+
+func Serve() {
+
+}
+
+func Start() {
+
+}
+
+func Stop() {
+
+}
