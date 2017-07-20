f9c084de252bf8a393fd9cb4fc51dccf717aaa3c
Anastasiya Ashaeva <doctorcotic@MacBook-Pro-Anastasiya.local>
2017-07-20 17:30:07 +0300

    readme

---
+++ StructureMetrics.go
@@ -0,0 +1,47 @@
+package metricsStructs
+
+import (
+	"time"
+	//"fmt"
+
+)
+
+type MetricsHub struct {
+	HubAddress string `json:"hubAddress"`
+	HubPing string `json: "hubPing"`
+	HubService string `json: "hubService"`
+	HubStack string `json: "hubStack"`
+	CreationDate string `json: "creationDate"`
+
+	//HubLifetime time.Time
+	//PayDay string
+	//FreezeTime time.Time
+	//AmountFreezeTime int
+	//TransferLimit string
+	//SuspectStatus bool
+	//DayLimit time.Time
+	//AvailabilityPresale bool
+	//SpeedConfirm time.Time
+}
+
+type MetricsMin struct {
+	MinAddress string
+	MinPing string
+	MinStack string
+	CreationDate time.Time
+	MinService string
+}
+
+func (metricsHub *MetricsHub) StructureMetrics  () {
+	//v := MetricsHub{"hubAddress", "HubPing", "HubService", "HubStack", time.Now()}
+	//v.HubAddress = "CurrentlyHub address"
+	//fmt.Print(v.HubAddress)
+}
+
+
+//func bestHub (){
+//
+//}
+//func bestMiner (){
+//
+//}
