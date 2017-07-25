8020f217567ae2aa26fc3f554685ff2573590890
Alexandr Nikonov <alex@sshaman.ru>
2017-07-25 13:00:37 +0300

    Fixing select statement usage for handling exit message

--- examples/fusrodah/main.go
+++ examples/fusrodah/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/sonm-io/fusrodah/fusrodah"
-	"github.com/ethereum/go-ethereum/whisper/whisperv2"
 	"time"
+
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/whisper/whisperv2"
+	"github.com/sonm-io/fusrodah/fusrodah"
 )
 
 func main() {
@@ -13,27 +14,29 @@ func main() {
 	prv, _ := crypto.GenerateKey()
 
 	frd := fusrodah.Fusrodah{
-		Prv:   prv,
-		Port:  ":30345",
-		Enode: "enode://b0605764bd7c6a816c51325a9cb9d414277d639f420f9dc48b20d12c04c33391b0a99cc8c045d7ba4657de0c04e8bb3b0d4b072ca9779167a75761d7c3c18eb0@10.196.131.151:30348",
+		Prv:  prv,
+		Port: ":30345",
+		// Enode is bootnode p2p addr
+		Enode: "enode://12428dbfae0929ef05a6bfe2798db9ad830b9d65f74aea98285ffa105196ebd6b823ed4911fc5b65b3a4868b23f47f1e577e48b4b833f30cc1fbdaa0ff4d21bf@172.16.1.189:30348",
 	}
 
 	frd.Start()
 
 	frd.AddHandling(nil, nil, func(msg *whisperv2.Message) {
-		fmt.Println(string(msg.Payload))
+		fmt.Printf("Incoming message: %s\r\n", string(msg.Payload))
 		if string(msg.Payload) == "Quit" {
 			close(done)
 		}
-	}, "test")
+	})
 
-	for{
-		time.Sleep(3*time.Second)
-		frd.Send("Quit", nil, true, "test")
-	}
-
-	select {
-	case <-done:
-
+	for {
+		select {
+		case <-done:
+			fmt.Println("Quit command received, finishing...")
+			return
+		default:
+			time.Sleep(3 * time.Second)
+			frd.Send("healthz", nil, true)
+		}
 	}
 }
