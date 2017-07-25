bcdb5d2af9f8677d81efa2040f16011261e6f251
DoctorCotic <neonneoxin60@outlook.com>
2017-07-25 14:03:17 +0300

    add serialise & de_serialise structure

--- StructureMetrics.go
+++ StructureMetrics.go
@@ -2,38 +2,46 @@ package metricsStructs
 
 import (
 	"time"
-	//"fmt"
-
+	"encoding/json"
 )
 
+// MetricsHub describe some
 type MetricsHub struct {
-	HubAddress string `json:"hubAddress"`
-	HubPing string `json: "hubPing"`
-	HubService string `json: "hubService"`
-	HubStack string `json: "hubStack"`
-	CreationDate string `json: "creationDate"`
-
-	//HubLifetime time.Time
-	//PayDay string
-	//FreezeTime time.Time
-	//AmountFreezeTime int
-	//TransferLimit string
-	//SuspectStatus bool
-	//DayLimit time.Time
-	//AvailabilityPresale bool
-	//SpeedConfirm time.Time
+	HubAddress          string        `json:"hubAddress"`
+	HubPing             string        `json:"hubPing"`
+	HubService          string        `json:"hubService"`
+	HubStack            string        `json:"hubStack"`
+	CreationDate        string        `json:"creationDate"`
+	HubLifetime         time.Time     `json:"hubLifeTime"`
+	PayDay              string        `json:"payDay"`
+	FreezeTime          time.Time     `json:"freezeTime"`
+	AmountFreezeTime    int 		  `json:"amountFreezeTime"`
+	TransferLimit       string 		  `json:"transferLimit"`
+	SuspectStatus       bool 		  `json:"suspectStatus"`
+	DayLimit            time.Time 	  `json:"dayLimit"`
+	AvailabilityPresale bool 		  `json:"availabilityPresale"`
+	SpeedConfirm        time.Time 	  `json:"speedConfirm"`
 }
-
-type MetricsMin struct {
-	MinAddress string
-	MinPing string
-	MinStack string
+type MetricsMiner struct {
+	MinAddress   string
+	MinPing      string
+	MinStack     string
 	CreationDate time.Time
-	MinService string
+	MinService   string
 }
 
-func (metricsHub *MetricsHub) StructureMetrics  () {
-	//v := MetricsHub{"hubAddress", "HubPing", "HubService", "HubStack", time.Now()}
-	//v.HubAddress = "CurrentlyHub address"
-	//fmt.Print(v.HubAddress)
+// ToJSON serialise struct
+func (m *MetricsHub) ToJSON() []byte {
+	b, _ := json.Marshal(m)
+	return b
 }
+
+//FromJSON de_serialise struct
+func (m *MetricsHub) FromJSON(b []byte) error {
+	err := json.Unmarshal(b, &m)
+	return err
+}
+
+//func (m *MetricsHub) MarshalJson() ([]byte, error) {
+//	return []byte (`{"hubAddress": "` + m.HubAddress + `"}`), nil
+//}
--- main/test.go
+++ main/test.go
@@ -3,15 +3,45 @@ package main
 import (
 	"github.com/sonm-io/metricsStructs"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"encoding/json"
 )
 
-/**
- /--------TEST--------/
- THIS FUNCTION FOR TEST
- /--------------------/
-*/
+type jsonobject struct {
+	Object metricsStructs.MetricsHub
+}
+
 func main() {
-	v := metricsStructs.MetricsHub{"hubAddress", "HubPing", "HubService", "HubStack", "date"}
-	v.HubAddress = "CurrentlyHub address"
-	fmt.Print(v.HubAddress)
+	json.NewEncoder(os.Stdout).Encode("./metricsStructs/config.json")
+
+	file, err := ioutil.ReadFile("./metricsStructs/config.json")
+	if err != nil {
+		fmt.Print("File error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Read json file: %s\n", string(file))
+
+	metricsHubVar1 := &metricsStructs.MetricsHub{
+		HubAddress:   "12345",
+		HubPing:      "1234556",
+		HubService:   "sercive",
+		HubStack:     "dfdws",
+		CreationDate: "ddsv",
+	}
+
+	to := metricsHubVar1.ToJSON()
+	fmt.Printf("Result: %s", string(to))
+
+	b := []byte(file)
+
+	from := metricsHubVar1.FromJSON(b)
+	fmt.Printf("Result from JSON: %s", string(from))
+
+	//m := map[string]*metricsStructs.MetricsHub
+	//m["c"] = metricsHubVar1
+
+	//metricsHubVar2, _ := json.Marshal(metricsHubVar1)
+	//fmt.Printf("Standart marshal structure: %s\n",string(metricsHubVar2))
+
 }
