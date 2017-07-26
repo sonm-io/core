34f20b5ab74870fd372efe441609826ac1a2b95a
Alexandr Nikonov <alex@sshaman.ru>
2017-07-26 19:00:40 +0300

    Updating bootnode addr

--- common/const.go
+++ common/const.go
@@ -1,7 +1,7 @@
 package common
 
 const (
-	BootNodeAddr = "enode://511a163bd31d175e8a86d5fbc98e5398a1c1556f8e283a3df777488da03568959c9758a04bc2108cdccc0e5f216a9529dcc1532224028ff3df328604a105f772@10.0.0.5:30348"
+	BootNodeAddr = "enode://aaff8b588b3c2bde265504c78cb33449d8b5659bc172278e50abc73a2521fbd938aefd097c077efae316754b115f4e27b5126c6785cd51932331ba224c8734c2@82.146.58.183:30348"
 
 	TopicHubDiscover   = "hubDiscover"
 	TopicMinerDiscover = "minerDiscover"
