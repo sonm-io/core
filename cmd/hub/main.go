216951095b44ad28d9896c9448e291657e5026ba
Alexandr Nikonov <alex@sshaman.ru>
2017-07-31 12:23:17 +0300

    Calling os.Exit(1) explicitly

--- cmd/hub/main.go
+++ cmd/hub/main.go
@@ -23,13 +23,14 @@ func main() {
 
 	conf, err := hub.NewConfig(*configPath)
 	if err != nil {
-		ctxlog.GetLogger(ctx).Fatal("Cannot load config", zap.Error(err))
+		ctxlog.GetLogger(ctx).Error("Cannot load config", zap.Error(err))
 		os.Exit(1)
 	}
 
 	h, err := hub.New(ctx, conf)
 	if err != nil {
-		ctxlog.GetLogger(ctx).Fatal("failed to create a new Hub", zap.Error(err))
+		ctxlog.GetLogger(ctx).Error("failed to create a new Hub", zap.Error(err))
+		os.Exit(1)
 	}
 	go func() {
 		c := make(chan os.Signal, 1)
