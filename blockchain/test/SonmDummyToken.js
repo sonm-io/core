aa1a9e1b2701fc6ff03174bf87b2a8a17717876b
Antony Shchukin <szwtdy@gmail.com>
2017-08-02 02:36:28 +0300

    feat: add SonmDummyToken tests

---
+++ blockchain/test/SonmDummyToken.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const assertJump = require('./helpers/assertJump');
+var SonmDummyToken = artifacts.require('../contracts/SonmDummyToken.sol');
+
+let initial_supply = 1000000;
+
+contract('SonmDummyToken', function (accounts) {
+
+    it('should return the correct totalSupply after construction', async function () {
+        let token = await SonmDummyToken.new(accounts[0]);
+        let totalSupply = await token.totalSupply();
+
+        assert.equal(totalSupply, initial_supply);
+    });
+
+    it('should return the correct allowance amount after approval', async function () {
+        let token = await SonmDummyToken.new(accounts[0]);
+        await token.approve(accounts[1], 100);
+        let allowance = await token.allowance(accounts[0], accounts[1]);
+
+        assert.equal(allowance, 100);
+    });
+
+    it('should return correct balances after transfer', async function () {
+        let token = await SonmDummyToken.new(accounts[0]);
+        await token.transfer(accounts[1], 100);
+        let balance0 = await token.balanceOf(accounts[0]);
+        assert.equal(balance0, initial_supply - 100);
+
+        let balance1 = await token.balanceOf(accounts[1]);
+        assert.equal(balance1, 100);
+    });
+
+    it('should throw an error when trying to transfer more than balance', async function () {
+        let token = await SonmDummyToken.new(accounts[0]);
+        let balance1 = await token.balanceOf(accounts[1]);
+        try {
+            await token.transfer(accounts[1], balance1 + 1);
+        } catch (error) {
+            return assertJump(error);
+        }
+        assert.fail('should have thrown before');
+    });
+
+    it('should return correct balances after transfering from another account', async function () {
+        let token = await SonmDummyToken.new(accounts[0]);
+        await token.approve(accounts[1], 100);
+        await token.transferFrom(accounts[0], accounts[2], 100, {from: accounts[1]});
+
+        let balance0 = await token.balanceOf(accounts[0]);
+        assert.equal(balance0, initial_supply - 100);
+
+        let balance1 = await token.balanceOf(accounts[2]);
+        assert.equal(balance1, 100);
+
+        let balance2 = await token.balanceOf(accounts[1]);
+        assert.equal(balance2, 0);
+    });
+
+    it('should throw an error when trying to transfer more than allowed', async function() {
+      let token = await SonmDummyToken.new();
+      await token.approve(accounts[1], 99);
+      try {
+        await token.transferFrom(accounts[0], accounts[2], 100, {from: accounts[1]});
+      } catch (error) {
+        return assertJump(error);
+      }
+      assert.fail('should have thrown before');
+    });
+
+});
