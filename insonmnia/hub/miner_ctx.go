e9362717a23c9751e2c5b0188b8a80d1fed7b1ce
Anton Matveenko <antmat@me.com>
2017-08-06 17:18:13 +0300

    fix: restore MinerStatus behaviour

--- insonmnia/hub/miner_ctx.go
+++ insonmnia/hub/miner_ctx.go
@@ -88,23 +88,23 @@ func (m *MinerCtx) initStatusClient() (statusClient pbminer.Miner_TasksStatusCli
 	return
 }
 
-// NOTE: maybe rewrite this method as poller (like ping() func)
-func (m *MinerCtx) fetchStatuses() error {
+func (m *MinerCtx) pollStatuses() error {
 	statusClient, err := m.initStatusClient()
 	if err != nil {
 		return err
 	}
 
-	statusReply, err := statusClient.Recv()
-	if err != nil {
-		log.G(m.ctx).Info("failed to receive miner status", zap.Error(err))
-		return err
-	}
+	for {
+		statusReply, err := statusClient.Recv()
+		if err != nil {
+			log.G(m.ctx).Error("failed to receive miner status", zap.Error(err))
+			return err
+		}
 
-	m.status_mu.Lock()
-	m.status_map = statusReply.Statuses
-	m.status_mu.Unlock()
-	return nil
+		m.status_mu.Lock()
+		m.status_map = statusReply.Statuses
+		m.status_mu.Unlock()
+	}
 }
 
 func (m *MinerCtx) ping() error {
