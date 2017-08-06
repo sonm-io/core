58bbd394d26595af3808f0019d9b8c93f51b6e11
Evgeny Safronov <division494@gmail.com>
2017-08-06 21:31:20 +0300

    feat: collect OS resources on Miner
    Finally closes #5.

--- insonmnia/miner/server_test.go
+++ insonmnia/miner/server_test.go
@@ -2,7 +2,10 @@ package miner
 
 import (
 	"context"
+	"errors"
+	"github.com/cloudfoundry/gosigar"
 	"github.com/golang/mock/gomock"
+	"github.com/sonm-io/core/insonmnia/resource"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -17,6 +20,38 @@ func TestServerNewExtractsHubEndpoint(t *testing.T) {
 	cfg.EXPECT().HubResources().AnyTimes()
 	m, err := New(ctx, cfg)
 
+	assert.NotNil(t, m)
 	assert.Nil(t, err)
 	assert.Equal(t, "::1", m.hubAddress)
+}
+
+func TestServerNewFailsWhenFailedCollectResources(t *testing.T) {
+	mock := gomock.NewController(t)
+	defer mock.Finish()
+
+	ctx := context.Background()
+	cfg := NewMockConfig(mock)
+	collector := resource.NewMockCollector(mock)
+	collector.EXPECT().OS().Times(1).Return(nil, errors.New(""))
+	m, err := newMiner(ctx, cfg, collector)
+
+	assert.Nil(t, m)
+	assert.Error(t, err)
+}
+
+func TestServerNewSavesResources(t *testing.T) {
+	mock := gomock.NewController(t)
+	defer mock.Finish()
+
+	ctx := context.Background()
+	cfg := NewMockConfig(mock)
+	cfg.EXPECT().HubEndpoint().AnyTimes()
+	cfg.EXPECT().HubResources().AnyTimes()
+	collector := resource.NewMockCollector(mock)
+	collector.EXPECT().OS().Times(1).Return(&resource.OS{CPU: sigar.CpuList{}, Mem: sigar.Mem{Total: 42}}, nil)
+	m, err := newMiner(ctx, cfg, collector)
+
+	assert.NotNil(t, m)
+	assert.Nil(t, err)
+	assert.Equal(t, uint64(42), m.resources.Mem.Total)
 }
