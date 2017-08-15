47475676a06021bbc5f7f3cdc037ff1376a0ada3
Antony Shchukin <szwtdy@gmail.com>
2017-08-15 13:33:20 +0300

    chore: recover examples

--- fusrodah/examples/miner/main.go
+++ fusrodah/examples/miner/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	"github.com/sonm-io/core/fusrodah/miner"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func main() {
+	prv, _ := crypto.GenerateKey()
 
-	srv, err := miner.NewServer(nil)
+	srv, err := miner.NewServer(prv)
 	if err != nil {
 		fmt.Printf("Error while initialize instanse: %s \r\n", err)
 		return
