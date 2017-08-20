59f8046efd62fce0f651adef99275049254987da
Evgeny Safronov <division494@gmail.com>
2017-08-21 02:41:18 +0300

    feat: port pool for IPVS

---
+++ insonmnia/gateway/pool.go
@@ -0,0 +1,59 @@
+package gateway
+
+import (
+	"github.com/pkg/errors"
+	"gopkg.in/oleiade/lane.v1"
+	"math/rand"
+	"sync"
+)
+
+type PortPool struct {
+	queue *lane.Queue
+	used  map[string]uint16
+	mu    sync.Mutex
+}
+
+func NewPortPool(init, size uint16) *PortPool {
+	p := &PortPool{
+		queue: lane.NewQueue(),
+	}
+
+	for _, v := range rand.Perm(int(size)) {
+		p.queue.Enqueue(init + uint16(v))
+	}
+
+	return p
+}
+
+func (p *PortPool) Assign(ID string) (uint16, error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if _, exists := p.used[ID]; exists {
+		return 0, errors.New("named port is already in use")
+	}
+
+	port := p.queue.Dequeue()
+	if port == nil {
+		return 0, errors.New("no ports left for allocation")
+	}
+
+	p.used[ID] = port.(uint16)
+
+	return port.(uint16), nil
+}
+
+func (p *PortPool) Retain(ID string) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if port, exists := p.used[ID]; exists {
+		p.queue.Enqueue(port)
+	} else {
+		return errors.New("named port was never assigned")
+	}
+
+	delete(p.used, ID)
+
+	return nil
+}
