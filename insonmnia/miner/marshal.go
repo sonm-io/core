15c4dbacc5dd43a732f6386bd2628a9856326216
Evgeny Safronov <division494@gmail.com>
2017-08-20 20:54:03 +0300

    feat: provide NAT type info to a hub

---
+++ insonmnia/miner/marshal.go
@@ -0,0 +1,31 @@
+package miner
+
+import (
+	"github.com/ccding/go-stun/stun"
+
+	pb "github.com/sonm-io/core/proto"
+)
+
+func MarshalNATType(nat stun.NATType) pb.NATType {
+	switch nat {
+	case stun.NATNone:
+		return pb.NATType_NONE
+	case stun.NATError:
+	case stun.NATUnknown:
+		return pb.NATType_UNKNOWN
+	case stun.NATBlocked:
+		return pb.NATType_BLOCKED
+	case stun.NATFull:
+		return pb.NATType_FULL
+	case stun.NATSymetric:
+		return pb.NATType_SYMMETRIC
+	case stun.NATRestricted:
+		return pb.NATType_RESTRICTED
+	case stun.NATPortRestricted:
+		return pb.NATType_PORT_RESTRICTED
+	case stun.NATSymetricUDPFirewall:
+		return pb.NATType_SYMMETRIC_UDP_FIREWALL
+	}
+
+	return pb.NATType_UNKNOWN
+}
