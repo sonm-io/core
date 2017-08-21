6ac848aaf99b1257780a1ccd21eb2434b4862750
Evgeny Safronov <division494@gmail.com>
2017-08-21 18:34:47 +0300

    feat: remap services properly

--- insonmnia/hub/router_linux.go
+++ insonmnia/hub/router_linux.go
@@ -18,34 +18,43 @@ func newIPVSRouter(ctx context.Context, gate *gateway.Gateway) router {
 	}
 }
 
-func (r *ipvsRouter) RegisterRoute(ID string, protocol string, realIP string, realPort uint16) error {
+func (r *ipvsRouter) RegisterRoute(ID string, protocol string, realIP string, realPort uint16) (*route, error) {
 	host, err := gateway.GetOutboundIP()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	port, err := r.pool.Assign(ID)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	serviceOptions, err := gateway.NewServiceOptions(host.String(), port, protocol)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if err := r.gateway.CreateService(ID, serviceOptions); err != nil {
-		return err
+		return nil, err
 	}
 
 	realOptions, err := gateway.NewRealOptions(realIP, realPort, 100, ID)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if err := r.gateway.CreateBackend(ID, ID, realOptions); err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	route := &route{
+		ID:          ID,
+		Protocol:    protocol,
+		Host:        host,
+		Port:        port,
+		BackendHost: realIP,
+		BackendPort: realPort,
+	}
+
+	return route, nil
 }
