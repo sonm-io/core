b277697590c74fe489d5a4b5ec0c14b86018cc3d
Evgeny Safronov <division494@gmail.com>
2017-08-26 02:13:56 +0300

    feat: full metrics dump

--- insonmnia/gateway/metrics.go
+++ insonmnia/gateway/metrics.go
@@ -3,12 +3,28 @@ package gateway
 // Metrics describes a virtual service metrics.
 type Metrics struct {
 	Connections uint64
+	InPackets   uint64
+	OutPackets  uint64
 	InBytes     uint64
 	OutBytes    uint64
+
+	ConnectionsPerSecond uint64
+	InPacketsPerSecond   uint64
+	OutPacketsPerSecond  uint64
+	InBytesPerSecond     uint64
+	OutBytesPerSecond    uint64
 }
 
 func (m *Metrics) Add(metrics *Metrics) {
 	m.Connections += metrics.Connections
+	m.InPackets += metrics.InPackets
+	m.OutPackets += metrics.OutPackets
 	m.InBytes += metrics.InBytes
 	m.OutBytes += metrics.OutBytes
+
+	m.ConnectionsPerSecond += metrics.ConnectionsPerSecond
+	m.InPacketsPerSecond += metrics.InPacketsPerSecond
+	m.OutPacketsPerSecond += metrics.OutPacketsPerSecond
+	m.InBytesPerSecond += metrics.InBytesPerSecond
+	m.OutBytesPerSecond += metrics.OutBytesPerSecond
 }
