0389a630a4d804662c3a94da91a4af8e2fd2e76d
Anton Tiurin <noxiouz@yandex.ru>
2017-11-13 23:18:28 +0300

    grcp: fix TLS Mutual Auth
    Add TLSConnection Test

--- util/grpcutil_test.go
+++ util/grpcutil_test.go
@@ -1,9 +1,18 @@
 package util
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"net"
 	"testing"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/stretchr/testify/require"
 
 	ethcrypto "github.com/ethereum/go-ethereum/crypto"
 )
@@ -29,4 +38,52 @@ func TestTLSGenCerts(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestSecureGRPCConnect(t *testing.T) {
+	require := require.New(t)
+	ctx := context.Background()
+	serPriv, err := ethcrypto.GenerateKey()
+	require.NoError(err)
+	rot, serTLS, err := NewHitlessCertRotator(ctx, serPriv)
+	require.NoError(err)
+	defer rot.Close()
+	serCreds := NewTLS(serTLS)
+	server := MakeGrpcServer(serCreds)
+	lis, err := net.Listen("tcp", "localhost:0")
+	require.NoError(err)
+	defer lis.Close()
+	go func() {
+		server.Serve(lis)
+	}()
+
+	t.Run("ClientWithTLS", func(t *testing.T) {
+		clientPriv, err := ethcrypto.GenerateKey()
+		require.NoError(err)
+		rot, clientTLS, err := NewHitlessCertRotator(ctx, clientPriv)
+		require.NoError(err)
+		defer rot.Close()
+		var clientCreds = NewTLS(clientTLS)
+		conn, err := MakeGrpcClient(ctx, lis.Addr().String(), clientCreds, grpc.WithTimeout(time.Second), grpc.WithBlock())
+		require.NoError(err)
+		defer conn.Close()
+
+		err = grpc.Invoke(ctx, "/DummyService/dummyMethod", nil, nil, conn)
+		require.NotNil(err)
+		st, ok := status.FromError(err)
+		require.True(ok)
+		require.True(st.Code() == codes.Unimplemented)
+	})
+
+	t.Run("ClientWithoutTLS", func(t *testing.T) {
+		conn, err := MakeGrpcClient(ctx, lis.Addr().String(), nil, grpc.WithTimeout(time.Second), grpc.WithBlock())
+		require.NoError(err)
+		defer conn.Close()
+		require.NotNil(conn)
+		err = grpc.Invoke(ctx, "/DummyService/dummyMethod", nil, nil, conn)
+		require.NotNil(err)
+		st, ok := status.FromError(err)
+		require.True(ok)
+		require.True(st.Code() == codes.Internal)
+	})
 }
