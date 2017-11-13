219c42271d2441dfa7574f1d53cfda86d26272fa
Antony Shchukin <szwtdy@gmail.com>
2017-11-13 16:54:00 +0300

    upd: dependency

--- blockchain/example/example.go
+++ blockchain/example/example.go
@@ -36,12 +36,11 @@ func main() {
 		return
 	}
 
-	tx, err := bch.Approve(prv, "0x7Ca4d552Fc464912a4dd05112037A384C13072b7", big.NewInt(10000) );
+	tx, err := bch.Approve(prv, "0x7Ca4d552Fc464912a4dd05112037A384C13072b7", big.NewInt(10000))
 	if err != nil {
 		log.Fatalln(err)
 		return
 	}
-
 
 	//tx, err := bch.OpenDeal(prv,"0x41ba7e0e1e661f7114f2f05afd2536210c2ed351", "0x41ba7e0e1e661f7114f2f05afd2536210c2ed351", big.NewInt(1236782361542612), big.NewInt(10000), big.NewInt(3600))
 	//if err != nil {
