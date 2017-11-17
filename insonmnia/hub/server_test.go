d6945ab790bc76a0a725a47b4884b52641e910fa
Andrew Zavgorodniy <a.o.zavgorodny@gmail.com>
2017-11-17 22:23:39 +0300

    refactor: updated tests

--- insonmnia/hub/server_test.go
+++ insonmnia/hub/server_test.go
@@ -11,17 +11,52 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestDevices(t *testing.T) {
+	// GPU characteristics shared between miners.
+	gpuDevice, err := gpu.NewDevice("a", "b", 1488, 660)
+	assert.NoError(t, err)
+
+	hub := Hub{
+		miners: map[string]*MinerCtx{
+			"miner1": {
+				capabilities: &hardware.Hardware{
+					CPU: []cpu.Device{{CPU: 64}},
+					GPU: []gpu.Device{gpuDevice},
+				},
+			},
+			"miner2": {
+				capabilities: &hardware.Hardware{
+					CPU: []cpu.Device{{CPU: 65}},
+					GPU: []gpu.Device{gpuDevice},
+				},
+			},
+		},
+	}
+
+	devices, err := hub.Devices(context.Background(), &pb.Empty{})
+	assert.NoError(t, err)
+	assert.Equal(t, len(devices.CPUs), 2)
+	assert.Equal(t, len(devices.GPUs), 1)
+}
+
 func TestMinerDevices(t *testing.T) {
-	id := &pb.ID{Id: "test"}
+	id := &pb.ID{Id: "miner1"}
 
 	gpuDevice, err := gpu.NewDevice("a", "b", 1488, 660)
 	assert.NoError(t, err)
 
 	hub := Hub{
 		miners: map[string]*MinerCtx{
-			id.Id: &MinerCtx{
+			id.Id: {
 				capabilities: &hardware.Hardware{
 					CPU: []cpu.Device{{CPU: 64}},
+					GPU: []gpu.Device{gpuDevice},
+				},
+			},
+
+			"miner2": {
+				capabilities: &hardware.Hardware{
+					CPU: []cpu.Device{{CPU: 65}},
 					GPU: []gpu.Device{gpuDevice},
 				},
 			},
