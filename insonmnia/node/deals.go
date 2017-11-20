edbf33d290fa0f75bd101a8906b5904aa34cc05a
Antony Shchukin <szwtdy@gmail.com>
2017-11-20 17:58:25 +0300

    upd: deals contract subtree  & deals pb structs

--- insonmnia/node/deals.go
+++ insonmnia/node/deals.go
@@ -26,24 +26,17 @@ func (d *dealsAPI) List(ctx context.Context, req *pb.DealListRequest) (*pb.DealL
 
 	deals := make([]*pb.Deal, 0, len(IDs))
 	for _, id := range IDs {
-		info, err := d.bc.GetDealInfo(id)
+		deal, err := d.bc.GetDealInfo(id)
 		if err != nil {
 			return nil, err
 		}
 
 		// filter by status
-		if req.Status != pb.DealStatus_ANY_STATUS && req.Status != pb.DealStatus(info.Status.Int64()) {
+		if req.Status != pb.DealStatus_ANY_STATUS && req.Status != pb.DealStatus(deal.Status) {
 			continue
 		}
 
-		deals = append(deals, &pb.Deal{
-			Price:       info.Price.Int64(),
-			Status:      pb.DealStatus(info.Status.Int64()),
-			BuyerID:     info.Client,
-			SuppclierID: info.Hub,
-			StartTime:   &pb.Timestamp{Seconds: info.StartTime.Int64()},
-			EndTime:     &pb.Timestamp{Seconds: info.EndTime.Int64()},
-		})
+		deals = append(deals, deal)
 	}
 
 	return &pb.DealListReply{Deal: deals}, nil
@@ -56,21 +49,7 @@ func (d *dealsAPI) Status(ctx context.Context, id *pb.ID) (*pb.Deal, error) {
 		return nil, err
 	}
 
-	info, err := d.bc.GetDealInfo(bigID)
-	if err != nil {
-		return nil, err
-	}
-
-	deal := &pb.Deal{
-		Price:       info.Price.Int64(),
-		Status:      pb.DealStatus(info.Status.Int64()),
-		BuyerID:     info.Client,
-		SuppclierID: info.Hub,
-		StartTime:   &pb.Timestamp{Seconds: info.StartTime.Int64()},
-		EndTime:     &pb.Timestamp{Seconds: info.EndTime.Int64()},
-	}
-
-	return deal, nil
+	return d.bc.GetDealInfo(bigID)
 }
 
 func (d *dealsAPI) Finish(ctx context.Context, id *pb.ID) (*pb.Empty, error) {
