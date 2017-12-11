a5391b5d5a3c5f01670f45003e7d49ba9a839ebf
Antony Shchukin <szwtdy@gmail.com>
2017-12-11 18:50:08 +0300

    fix: set approved value to zero befora changing the value

--- cmd/cli/commands/tokens.go
+++ cmd/cli/commands/tokens.go
@@ -1,8 +1,11 @@
 package commands
 
 import (
+	"math/big"
 	"os"
 
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/sonm-io/core/blockchain"
 	"github.com/sonm-io/core/blockchain/tsc"
 	"github.com/sonm-io/core/util"
@@ -15,6 +18,8 @@ var approveTokenCmd = &cobra.Command{
 	PreRun: loadKeyStoreWrapper,
 	Args:   cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		var zero = big.NewInt(0)
+
 		bch, err := blockchain.NewAPI(nil, nil)
 		if err != nil {
 			showError(cmd, "Cannot create blockchain connection", err)
@@ -27,9 +32,24 @@ var approveTokenCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
+		currentAllowance, err := bch.AllowanceOf(crypto.PubkeyToAddress(sessionKey.PublicKey).String(), tsc.DealsAddress)
+		if err != nil {
+			showError(cmd, "Cannot get allowance ", err)
+			os.Exit(1)
+		}
+
+		if currentAllowance.Cmp(zero) != 0 {
+			_, err = bch.Approve(sessionKey, tsc.DealsAddress, zero)
+			if err != nil {
+				showError(cmd, "Cannot set approved value to zero", err)
+				os.Exit(1)
+			}
+		}
+
 		tx, err := bch.Approve(sessionKey, tsc.DealsAddress, amount)
 		if err != nil {
 			showError(cmd, "Cannot approve tokens", err)
+			os.Exit(1)
 		}
 
 		showJSON(cmd, convertTransactionInfo(tx))
