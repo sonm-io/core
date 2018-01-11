f6f590eb39784ba356854ece006b669c47b8f123
Evgeny Safronov <division494@gmail.com>
2018-01-11 21:10:55 +0300

    fix: okay

--- insonmnia/hub/server.go
+++ insonmnia/hub/server.go
@@ -1343,7 +1343,9 @@ func New(ctx context.Context, cfg *Config, version string, opts ...Option) (*Hub
 		auth.Allow("StartTask").With(newDealAuthorization(ctx, h, newFieldDealMetaData())),
 		auth.Allow("TaskLogs").With(newDealAuthorization(ctx, h, newStringerFieldMetaData(h))),
 		auth.Allow("PushTask").With(newDealAuthorization(ctx, h, newContextDealMetaData())),
-		auth.Allow("PullTask").With(newDealAuthorization(ctx, h, newNamedStringerFieldMetaData(h, "TaskId"))),
+		// TODO: Tactical architectural solution.
+		// auth.Allow("PullTask").With(newDealAuthorization(ctx, h, newNamedStringerFieldMetaData(h, "TaskId"))),
+		auth.Allow("PullTask").With(auth.NewNilAuthorization()),
 		auth.WithFallback(auth.NewDenyAuthorization()),
 	)
 
