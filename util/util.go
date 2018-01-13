fe70d4b1e492662cedc6475a98fb66e42d929d4d
Alex Nikonov <alex@sshaman.ru>
2018-01-13 21:57:25 +0300

    fix: check if IP is either loopback or link-local (#382)

--- util/util.go
+++ util/util.go
@@ -141,17 +141,19 @@ func IsPrivateIP(ip net.IP) bool {
 }
 
 func isPrivateIPv4(ip net.IP) bool {
-	private := false
 	_, private24BitBlock, _ := net.ParseCIDR("10.0.0.0/8")
 	_, private20BitBlock, _ := net.ParseCIDR("172.16.0.0/12")
 	_, private16BitBlock, _ := net.ParseCIDR("192.168.0.0/16")
-	private = private24BitBlock.Contains(ip) || private20BitBlock.Contains(ip) || private16BitBlock.Contains(ip)
-
-	return private
+	return private24BitBlock.Contains(ip) ||
+		private20BitBlock.Contains(ip) ||
+		private16BitBlock.Contains(ip) ||
+		ip.IsLoopback() ||
+		ip.IsLinkLocalUnicast() ||
+		ip.IsLinkLocalMulticast()
 }
 
 func isPrivateIPv6(ip net.IP) bool {
 	_, block, _ := net.ParseCIDR("fc00::/7")
 
-	return block.Contains(ip)
+	return block.Contains(ip) || ip.IsLoopback() || ip.IsLinkLocalUnicast() || ip.IsLinkLocalMulticast()
 }
