fec8d3cf07a11381596517ff2ced1ff29771eaba
Evgeny Safronov <division494@gmail.com>
2018-01-21 13:33:13 +0300

    fix: copy-paste (#402)

--- insonmnia/hub/server.go
+++ insonmnia/hub/server.go
@@ -911,7 +911,7 @@ func (h *Hub) ApproveDeal(ctx context.Context, request *pb.ApproveDealRequest) (
 	}
 
 	usage, err := miner.OrderUsage(orderID)
-	if miner == nil {
+	if err != nil {
 		return nil, err
 	}
 
