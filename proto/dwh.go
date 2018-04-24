e225a076d9e66013305c13ea20999994fdaef2e6
Anton Matveenko <antmat@me.com>
2018-04-24 17:49:27 +0300

    refact: benchmarks are again array(with accessors)

--- proto/dwh.go
+++ proto/dwh.go
@@ -1,50 +1,22 @@
 package sonm
 
-import (
-	"github.com/pkg/errors"
-)
-
 const (
-	NumBenchmarks = 12
-	NumNetflags   = 3
+	NumNetflags = 3
 )
 
 func NewBenchmarks(benchmarks []uint64) (*Benchmarks, error) {
-	if len(benchmarks) < NumBenchmarks {
-		return nil, errors.Errorf("expected %d benchmarks, got %d", NumBenchmarks, len(benchmarks))
+	b := &Benchmarks{
+		Values: make([]uint64, len(benchmarks)),
 	}
-
-	return &Benchmarks{
-		CPUSysbenchMulti: benchmarks[0],
-		CPUSysbenchOne:   benchmarks[1],
-		CPUCores:         benchmarks[2],
-		RAMSize:          benchmarks[3],
-		StorageSize:      benchmarks[4],
-		NetTrafficIn:     benchmarks[5],
-		NetTrafficOut:    benchmarks[6],
-		GPUCount:         benchmarks[7],
-		GPUMem:           benchmarks[8],
-		GPUEthHashrate:   benchmarks[9],
-		GPUCashHashrate:  benchmarks[10],
-		GPURedshift:      benchmarks[11],
-	}, nil
+	copy(b.Values, benchmarks)
+	if err := b.Validate(); err != nil {
+		return nil, err
+	}
+	return b, nil
 }
 
 func (m *Benchmarks) ToArray() []uint64 {
-	return []uint64{
-		m.CPUSysbenchMulti,
-		m.CPUSysbenchOne,
-		m.CPUCores,
-		m.RAMSize,
-		m.StorageSize,
-		m.NetTrafficIn,
-		m.NetTrafficOut,
-		m.GPUCount,
-		m.GPUMem,
-		m.GPUEthHashrate,
-		m.GPUCashHashrate,
-		m.GPURedshift,
-	}
+	return m.Values
 }
 
 func UintToNetflags(flags uint64) [NumNetflags]bool {
--- proto/marketplace.go
+++ proto/marketplace.go
@@ -2,11 +2,15 @@ package sonm
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 	"time"
 )
 
-const MinDealDuration = time.Minute * 10
+const (
+	MinNumBenchmarks = 12
+	MinDealDuration  = time.Minute * 10
+)
 
 func (m *IdentityLevel) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	var v string
@@ -29,4 +33,66 @@ func (m *BidOrder) Validate() error {
 	}
 
 	return nil
+}
+
+func (m *Benchmarks) Validate() error {
+	if len(m.Values) < MinNumBenchmarks {
+		return fmt.Errorf("expected at least %d benchmarks, got %d", MinNumBenchmarks, len(m.Values))
+	}
+	return nil
+}
+
+func (m *Benchmarks) Get(idx int) uint64 {
+	if len(m.Values) <= idx {
+		return 0
+	}
+	return m.Values[idx]
+}
+
+func (m *Benchmarks) CPUSysbenchMulti() uint64 {
+	return m.Get(0)
+}
+
+func (m *Benchmarks) CPUSysbenchOne() uint64 {
+	return m.Get(1)
+}
+
+func (m *Benchmarks) CPUCores() uint64 {
+	return m.Get(2)
+}
+
+func (m *Benchmarks) RAMSize() uint64 {
+	return m.Get(3)
+}
+
+func (m *Benchmarks) StorageSize() uint64 {
+	return m.Get(4)
+}
+
+func (m *Benchmarks) NetTrafficIn() uint64 {
+	return m.Get(5)
+}
+
+func (m *Benchmarks) NetTrafficOut() uint64 {
+	return m.Get(6)
+}
+
+func (m *Benchmarks) GPUCount() uint64 {
+	return m.Get(7)
+}
+
+func (m *Benchmarks) GPUMem() uint64 {
+	return m.Get(8)
+}
+
+func (m *Benchmarks) GPUEthHashrate() uint64 {
+	return m.Get(9)
+}
+
+func (m *Benchmarks) GPUCashHashrate() uint64 {
+	return m.Get(10)
+}
+
+func (m *Benchmarks) GPURedshift() uint64 {
+	return m.Get(11)
 }
