7cff8fde59fc48a149753f42e64b4fea0936af05
Alex Nikonov <alex@sshaman.ru>
2018-05-04 13:25:21 +0300

    refactor: proper marshalling for EthAddress (#689)
    This PR implements `MarshalText` and `UnmarshalText` methods for the `EthAddress` type.
    It allows satisfying both JSON and YAML marshallers requirements.

--- proto/insonmnia_test.go
+++ proto/insonmnia_test.go
@@ -1,12 +1,15 @@
 package sonm
 
 import (
+	"encoding/json"
 	"math/big"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 )
 
 func TestConvertToPrice(t *testing.T) {
@@ -30,4 +33,62 @@ func TestConvertToPrice(t *testing.T) {
 		require.NoError(t, err)
 		assert.Equal(t, tt.expected, pr.PerSecond.Unwrap())
 	}
+}
+
+func TestEthAddress_MarshalText(t *testing.T) {
+	in := struct {
+		Addr *EthAddress `yaml:"addr" json:"addr"`
+	}{
+		Addr: NewEthAddress(common.HexToAddress("0x123")),
+	}
+
+	ya, err := yaml.Marshal(in)
+	require.NoError(t, err)
+
+	assert.Equal(t, "addr: \"0x0000000000000000000000000000000000000123\"\n", string(ya),
+		"YAML marshaller")
+
+	js, err := json.Marshal(in)
+	require.NoError(t, err)
+	assert.Equal(t, `{"addr":"0x0000000000000000000000000000000000000123"}`, string(js),
+		"JSON marshaller")
+}
+
+func TestEthAddress_MarshalTextByValue(t *testing.T) {
+	in := struct {
+		Addr EthAddress `yaml:"addr" json:"addr"`
+	}{}
+
+	tmp := NewEthAddress(common.HexToAddress("0x123"))
+	in.Addr = *tmp
+
+	ya, err := yaml.Marshal(in)
+	require.NoError(t, err)
+
+	assert.Equal(t, "addr: \"0x0000000000000000000000000000000000000123\"\n", string(ya),
+		"YAML marshaller")
+
+	js, err := json.Marshal(in)
+	require.NoError(t, err)
+	assert.Equal(t, `{"addr":"0x0000000000000000000000000000000000000123"}`, string(js),
+		"JSON marshaller")
+}
+
+func TestEthAddress_UnmarshalText(t *testing.T) {
+	in := []byte(`{"addr":"0x1234567891011121314151617181920212223242"}`)
+	recv := struct {
+		Addr *EthAddress `json:"addr" yaml:"addr"`
+	}{}
+
+	err := yaml.Unmarshal(in, &recv)
+	require.NoError(t, err)
+	assert.Equal(t, "0x1234567891011121314151617181920212223242", recv.Addr.Unwrap().Hex(),
+		"YAML marshall")
+
+	// reset prev value
+	recv.Addr = nil
+	err = json.Unmarshal(in, &recv)
+	require.NoError(t, err)
+	assert.Equal(t, "0x1234567891011121314151617181920212223242", recv.Addr.Unwrap().Hex(),
+		"JSON marshall")
 }
