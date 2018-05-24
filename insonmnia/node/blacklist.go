1e6cbac51b76e51c7a2edfa11c4fa541fdcaa8d6
Alexandr Nikonov <alex@sshaman.ru>
2018-05-24 19:14:35 +0300

    chore: do not wrap errors

--- insonmnia/node/blacklist.go
+++ insonmnia/node/blacklist.go
@@ -26,7 +26,7 @@ func (b *blacklistAPI) List(ctx context.Context, addr *sonm.EthAddress) (*sonm.B
 
 func (b *blacklistAPI) Remove(ctx context.Context, addr *sonm.EthAddress) (*sonm.Empty, error) {
 	if err := b.remotes.eth.Blacklist().Remove(ctx, b.remotes.key, addr.Unwrap()); err != nil {
-		return nil, errors.Wrap(err, "cannot remove address from blacklist")
+		return nil, errors.WithMessage(err, "cannot remove address from blacklist")
 	}
 
 	return &sonm.Empty{}, nil
