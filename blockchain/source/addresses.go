6f810c204aaeb492513dc6b893f967a5173c3314
Anton Shchukin <szwtdy@gmail.com>
2018-05-28 20:56:00 +0300

    aux: redeploy contracts (#861)
    * Deploy SNMD at sidechain.
    
    * Deploy SimpleGatekeeper to rinkeby chain.
    
    * Deploy SimpleGatekeeper to sidechain.
    
    Saving json artifacts of deployed contracts is necessary.

--- blockchain/source/addresses.go
+++ blockchain/source/addresses.go
@@ -3,11 +3,11 @@ package market
 import "github.com/ethereum/go-ethereum/common"
 
 // private chain
-const SNMSidechainAddress string = "0x5db024c6469634f4b307135cb76e8e6806f007b3"
+const SNMSidechainAddress string = "0x26524b1234e361eb4e3ddf7600d41271620fcb0a"
 
-const GatekeeperLiveAddress string = "0x8Ae371b723B2e32333FcfAD7733B6bbd64a4EA6F"
+const GatekeeperLiveAddress string = "0xbc29310be3693949094ce452b11829dbccca7d49"
 
-const GatekeeperSidechainAddress string = "0x74cd6dcaae93e61964f60d63925ac3853499b654"
+const GatekeeperSidechainAddress string = "0x9414922e778a0038058e9ea786e9474a89ad1ec0"
 
 // rinkeby
 const SNMAddress string = "0x06bda3cf79946e8b32a0bb6a3daa174b577c55b5"
