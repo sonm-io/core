9403d8fea327338dac1647332b7fe5c4927b19fc
quasisamurai <quazisamurai@gmail.com>
2018-05-28 20:54:45 +0300

    upd. migrations

--- blockchain/source/migrations/5_deploy_profileRegistry.js
+++ blockchain/source/migrations/5_deploy_profileRegistry.js
@@ -1,7 +1,8 @@
 var ProfileRegistry = artifacts.require("./ProfileRegistry.sol");
 
 module.exports = function (deployer, network) {
-    if (network !== "private") {
+    if (network !== "rinkeby") { //no reason to deploy to rinkeby
         deployer.deploy(ProfileRegistry);
     }
+
 };
