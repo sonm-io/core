d8dac8b736e450315076c5f3dec15c09b251d976
Antony Shchukin <szwtdy@gmail.com>
2018-05-29 19:24:44 +0300

    fix: regenerate abi-bindings
    Regenerate abi-bindings for all existing contracts.
    Corupted imports will be accidently changed by "Optimize Import" tool.

--- blockchain/source/api/OracleUSD.go
+++ blockchain/source/api/OracleUSD.go
@@ -7,12 +7,12 @@ import (
 	"math/big"
 	"strings"
 
-	ethereum "github.com/sonm-io/go-ethereum"
-	"github.com/sonm-io/go-ethereum/accounts/abi"
-	"github.com/sonm-io/go-ethereum/accounts/abi/bind"
-	"github.com/sonm-io/go-ethereum/common"
-	"github.com/sonm-io/go-ethereum/core/types"
-	"github.com/sonm-io/go-ethereum/event"
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
 )
 
 // OracleUSDABI is the input ABI used to generate the binding from.
