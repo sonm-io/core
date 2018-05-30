28d213010c58a43587052b64c11a7760068ac8d1
quasisamurai <quazisamurai@gmail.com>
2018-05-30 14:25:08 +0300

    linted migtations

--- blockchain/source/migrations/9_set_KYC.js
+++ blockchain/source/migrations/9_set_KYC.js
@@ -1,7 +1,7 @@
-var ProfileRegistry = artifacts.require("./ProfileRegistry.sol");
+var ProfileRegistry = artifacts.require('./ProfileRegistry.sol');
 
 module.exports = function (deployer, network) {
-    if (network === "private") {
+    if (network === 'private') {
         ProfileRegistry.deployed()
             .then(function (pr) {
                 pr.AddValidator('0x074dbe6017d6cb0bfb594046f694da8b7dc31266', 1);
@@ -9,5 +9,8 @@ module.exports = function (deployer, network) {
                 pr.AddValidator('0xbeeeff0a0f4dd2dbacfbf4ff4d4838962f761cc4', 3);
                 pr.AddValidator('0xac4b829daa17c686ac5264b70c9f4d9ce54a2ec9', 4);
             })
+            .catch(function (err) {
+                console.log(err);
+            });
     }
 };
