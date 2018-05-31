ff89010589d407a878fc997b10e4c123a7143030
Evgeny Safronov <division494@gmail.com>
2018-05-31 14:27:30 +0300

    fix: do not crash when the training set is degenerated (#911)

--- optimus/learning.go
+++ optimus/learning.go
@@ -99,6 +99,9 @@ type row = []float64
 type matrix = []row
 
 func transpose(m matrix) matrix {
+	if len(m) == 0 {
+		return m
+	}
 	r := make(matrix, len(m[0]))
 	for x := range r {
 		r[x] = make(row, len(m))
