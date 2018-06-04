500474a742774337fd918b1f0f52ee0edc3a316d
Anton Matveenko <antmat@me.com>
2018-06-04 17:34:24 +0300

    fix: use askplan hasher instead of hardware hasher for correct gpu index-hash mapping

--- insonmnia/worker/server.go
+++ insonmnia/worker/server.go
@@ -559,7 +559,8 @@ func (m *Worker) StartTask(ctx context.Context, request *pb.StartTaskRequest) (*
 		spec.Resources.GPU = ask.Resources.GPU
 	}
 
-	err = spec.GetResources().GetGPU().Normalize(m.hardware)
+	hasher := &pb.AskPlanHasher{ask.GetResources()}
+	err = spec.GetResources().GetGPU().Normalize(hasher)
 	if err != nil {
 		log.G(ctx).Error("could not normalize GPU resources", zap.Error(err))
 		m.setStatus(&pb.TaskStatusReply{Status: pb.TaskStatusReply_BROKEN}, taskID)
