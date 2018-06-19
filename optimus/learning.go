0ba64583219d9900335ee5198a9e52c964dacada
Evgeny Safronov <division494@gmail.com>
2018-06-19 18:13:17 +0300

    fix: do not null order created timestamp (#1027)
    When DWH was young it was unable to return properly created timestamp, we inited it ourselves.
    
    Now times change, but those empty initialization was forgotten and cause strange autosell bot behaviour.
    
    This commit removes such initialization.

--- optimus/learning.go
+++ optimus/learning.go
@@ -57,7 +57,9 @@ func (m *regressionClassifier) Classify(orders []*MarketOrder) ([]WeightedOrder,
 
 		distance := normalizer.Denormalize(normalizedPrice) - expectation[i]
 
-		orders[i].CreatedTS = &sonm.Timestamp{}
+		if orders[i].CreatedTS == nil {
+			orders[i].CreatedTS = &sonm.Timestamp{}
+		}
 		weightedOrders = append(weightedOrders, WeightedOrder{
 			Order:    orders[i],
 			Distance: distance,
