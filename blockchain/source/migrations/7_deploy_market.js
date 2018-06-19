c5d8cd0d9862defae8b91254614bdf69ea4218ee
Antony Shchukin <szwtdy@gmail.com>
2018-06-19 20:33:04 +0300

    redeploy contracts

--- blockchain/source/migrations/7_deploy_market.js
+++ blockchain/source/migrations/7_deploy_market.js
@@ -7,10 +7,10 @@ var ProfileRegistry = artifacts.require('./ProfileRegistry.sol');
 module.exports = function (deployer, network) {
     if (network === 'private') {
         deployer.deploy(Market,
-            '0x5db024c6469634f4b307135cb76e8e6806f007b3', // token address
-            '0x9ad1e969ec5842ee5d67414536813e224ceb56b1', // Blacklist address
+            SNM.address, // token address
+            Blacklist.address, // Blacklist address
             '0x1f995e52dcbec7c0d00d45b8b1bf43b29dd12b5b', // Oracle address
-            '0xacfe1a688649fe9798b44a76b906fdca6e584a8d', // ProfileRegistry address
+            ProfileRegistry.address, // ProfileRegistry address
             12, // benchmarks quantity
             3, // netflags quantity
             { gasPrice: 0 });
