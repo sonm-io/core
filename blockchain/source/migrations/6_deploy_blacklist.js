236ed865f07fa0951e37bdb82947e01d2ed5acf1
Antony Shchukin <szwtdy@gmail.com>
2018-06-20 21:43:57 +0300

    aux: redeploy all contracts

--- blockchain/source/migrations/6_deploy_blacklist.js
+++ blockchain/source/migrations/6_deploy_blacklist.js
@@ -3,6 +3,8 @@ var Blacklist = artifacts.require('./Blacklist.sol');
 module.exports = function (deployer, network) {
     if (network === 'private') {
         deployer.deploy(Blacklist, { gasPrice: 0 });
+    } else if (network === 'rinkeby') {
+        // blacklist haven't reason to deploy to rinkeby
     } else {
         deployer.deploy(Blacklist);
     }
