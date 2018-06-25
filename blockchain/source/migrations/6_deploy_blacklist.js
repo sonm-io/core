e74a9765f6c6a9f58831fa92747d5fab178dcb6a
Anton Shchukin <szwtdy@gmail.com>
2018-06-25 19:42:35 +0300

    chore: contracts migration scripts (#1068)
    * remaster deploy scripts

--- blockchain/source/migrations/6_deploy_blacklist.js
+++ blockchain/source/migrations/6_deploy_blacklist.js
@@ -1,8 +1,10 @@
-var Blacklist = artifacts.require('./Blacklist.sol');
+let Blacklist = artifacts.require('./Blacklist.sol');
 
 module.exports = function (deployer, network) {
-    if (network === 'private') {
+    if ((network === 'private') || (network === 'privateLive')) {
         deployer.deploy(Blacklist, { gasPrice: 0 });
+    } else if (network === 'master') {
+        // blacklist haven't reason to deploy to mainnet
     } else if (network === 'rinkeby') {
         // blacklist haven't reason to deploy to rinkeby
     } else {
