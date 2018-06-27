4a651a0b445dddc7d264564aa1a3eabfab69fe52
Evgeny Safronov <division494@gmail.com>
2018-06-27 13:57:05 +0300

    chore: more verbose cmd errors in Optimus

--- cmd/optimus/main.go
+++ cmd/optimus/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/sonm-io/core/cmd"
 	"github.com/sonm-io/core/insonmnia/logging"
@@ -21,13 +22,13 @@ func main() {
 func run() error {
 	cfg, err := optimus.LoadConfig(configFlag)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to parse config: %v", err)
 	}
 
 	ctx := context.Background()
 	bot, err := optimus.NewOptimus(*cfg, logging.BuildLogger(*cfg.Logging.Level))
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create Optimus: %v", err)
 	}
 
 	return bot.Run(ctx)
