2f204bb0b8bfdb001053695e8d95cd0ad35d6a3c
Antony Shchukin <szwtdy@gmail.com>
2018-06-28 15:37:59 +0300

    wip

--- blockchain/api.go
+++ blockchain/api.go
@@ -923,7 +923,7 @@ func NewProfileRegistry(address common.Address, opts *chainOpts) (ProfileRegistr
 	return &ProfileRegistry{
 		client:                  client,
 		profileRegistryContract: profileRegistryContract,
-		opts:                    opts,
+		opts: opts,
 	}, nil
 }
 
