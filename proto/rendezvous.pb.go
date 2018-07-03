raghu999/core-1

feat: rendezvous cluster mode

proto/rendezvous.pb.go
@@ -211,6 +211,7 @@ type RendezvousClient interface {
 	Publish(ctx context.Context, in *PublishRequest, opts ...grpc.CallOption) (*RendezvousReply, error)
 	// Info returns server's internal state.
 	Info(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*RendezvousState, error)
+	Discover(ctx context.Context, in *HandshakeRequest, opts ...grpc.CallOption) (*DiscoverResponse, error)
 }
 
 type rendezvousClient struct {
@@ -257,6 +258,15 @@ func (c *rendezvousClient) Info(ctx context.Context, in *Empty, opts ...grpc.Cal
 	return out, nil
 }
 
+func (c *rendezvousClient) Discover(ctx context.Context, in *HandshakeRequest, opts ...grpc.CallOption) (*DiscoverResponse, error) {
+	out := new(DiscoverResponse)
+	err := grpc.Invoke(ctx, "/sonm.Rendezvous/Discover", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // Server API for Rendezvous service
 
 type RendezvousServer interface {
@@ -275,6 +285,7 @@ type RendezvousServer interface {
 	Publish(context.Context, *PublishRequest) (*RendezvousReply, error)
 	// Info returns server's internal state.
 	Info(context.Context, *Empty) (*RendezvousState, error)
+	Discover(context.Context, *HandshakeRequest) (*DiscoverResponse, error)
 }
 
 func RegisterRendezvousServer(s *grpc.Server, srv RendezvousServer) {
@@ -353,6 +364,24 @@ func _Rendezvous_Info_Handler(srv interface{}, ctx context.Context, dec func(int
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Rendezvous_Discover_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(HandshakeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(RendezvousServer).Discover(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/sonm.Rendezvous/Discover",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(RendezvousServer).Discover(ctx, req.(*HandshakeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Rendezvous_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "sonm.Rendezvous",
 	HandlerType: (*RendezvousServer)(nil),
@@ -372,6 +401,10 @@ var _Rendezvous_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Info",
 			Handler:    _Rendezvous_Info_Handler,
+		},
+		{
+			MethodName: "Discover",
+			Handler:    _Rendezvous_Discover_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -463,6 +496,25 @@ var _Rendezvous_InfoCmd_gen = &cobra.Command{
 	RunE:  grpccmd.TypeToJson("sonm.Empty"),
 }
 
+var _Rendezvous_DiscoverCmd = &cobra.Command{
+	Use:   "discover",
+	Short: "Make the Discover method call, input-type: sonm.HandshakeRequest output-type: sonm.DiscoverResponse",
+	RunE: grpccmd.RunE(
+		"Discover",
+		"sonm.HandshakeRequest",
+		func(c io.Closer) interface{} {
+			cc := c.(*grpc.ClientConn)
+			return NewRendezvousClient(cc)
+		},
+	),
+}
+
+var _Rendezvous_DiscoverCmd_gen = &cobra.Command{
+	Use:   "discover-gen",
+	Short: "Generate JSON for method call of Discover (input-type: sonm.HandshakeRequest)",
+	RunE:  grpccmd.TypeToJson("sonm.HandshakeRequest"),
+}
+
 // Register commands with the root command and service command
 func init() {
 	grpccmd.RegisterServiceCmd(_RendezvousCmd)
@@ -475,6 +527,8 @@ func init() {
 		_Rendezvous_PublishCmd_gen,
 		_Rendezvous_InfoCmd,
 		_Rendezvous_InfoCmd_gen,
+		_Rendezvous_DiscoverCmd,
+		_Rendezvous_DiscoverCmd_gen,
 	)
 }
 
@@ -483,34 +537,36 @@ func init() {
 func init() { proto.RegisterFile("rendezvous.proto", fileDescriptor11) }
 
 var fileDescriptor11 = []byte{
-	// 453 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x53, 0xcd, 0x6a, 0xdb, 0x40,
-	0x10, 0xb6, 0xe4, 0xa4, 0x89, 0xc7, 0xc1, 0x51, 0x97, 0xfe, 0x08, 0x9d, 0x8c, 0xc8, 0x21, 0xf4,
-	0x47, 0x14, 0x17, 0x4a, 0xe8, 0xa1, 0x10, 0xa2, 0x1c, 0x74, 0x08, 0x34, 0x9b, 0x6b, 0x2f, 0x8a,
-	0x3c, 0x6d, 0x45, 0xd7, 0xbb, 0xaa, 0x76, 0x25, 0x50, 0x9f, 0xa5, 0x2f, 0xd2, 0xb7, 0xe9, 0xa3,
-	0x94, 0xdd, 0x95, 0x1c, 0xc9, 0x89, 0x31, 0x85, 0x5c, 0xec, 0xd1, 0xec, 0xf7, 0x33, 0x33, 0xbb,
-	0x03, 0x5e, 0x89, 0x7c, 0x89, 0xbf, 0x6a, 0x51, 0xc9, 0xa8, 0x28, 0x85, 0x12, 0x64, 0x4f, 0x0a,
-	0xbe, 0x0a, 0x8e, 0x73, 0xae, 0xff, 0x79, 0x9e, 0xda, 0x74, 0x30, 0xe1, 0xa8, 0x6c, 0x18, 0x32,
-	0x98, 0x5d, 0x08, 0xce, 0x31, 0x53, 0x14, 0x7f, 0x56, 0x28, 0x15, 0x99, 0x81, 0x9b, 0xc4, 0xbe,
-	0x33, 0x77, 0x4e, 0x27, 0xd4, 0x4d, 0x62, 0x12, 0xc0, 0xa1, 0x81, 0x66, 0x82, 0xf9, 0xae, 0xc9,
-	0xae, 0xbf, 0x49, 0x04, 0x47, 0x45, 0x99, 0xd7, 0xa9, 0xc2, 0xf3, 0xe5, 0xb2, 0x94, 0xfe, 0x78,
-	0x3e, 0x3e, 0x9d, 0x2e, 0x20, 0xd2, 0x76, 0x91, 0x4e, 0xd1, 0xc1, 0x79, 0xf8, 0x05, 0x66, 0x9f,
-	0xab, 0x5b, 0x96, 0xcb, 0xef, 0x9d, 0x5b, 0x5f, 0xdd, 0xd9, 0xa1, 0xee, 0xee, 0x50, 0x5f, 0xc1,
-	0x31, 0x5d, 0x4f, 0x80, 0x62, 0xc1, 0x1a, 0xf2, 0x0a, 0xa0, 0xd0, 0x86, 0x99, 0x46, 0x18, 0x83,
-	0xa1, 0x40, 0xef, 0xf4, 0xbf, 0xed, 0x7e, 0x3b, 0x7d, 0xbf, 0x1b, 0x95, 0x2a, 0x24, 0x1f, 0x60,
-	0x5f, 0xea, 0xc0, 0x77, 0x0c, 0x79, 0x6e, 0xc9, 0x1b, 0xa8, 0xc8, 0xfc, 0x5e, 0x72, 0x55, 0x36,
-	0xd4, 0xc2, 0x83, 0x6b, 0x80, 0xbb, 0x24, 0xf1, 0x60, 0xfc, 0x03, 0x9b, 0x76, 0x1e, 0x3a, 0x24,
-	0x6f, 0x61, 0xbf, 0x4e, 0x59, 0x85, 0xe6, 0x06, 0xa6, 0x8b, 0x97, 0x9b, 0xba, 0x57, 0x88, 0x2a,
-	0xe7, 0xdf, 0xa8, 0x45, 0x7d, 0x74, 0xcf, 0x9c, 0xf0, 0x8f, 0x0b, 0x4f, 0xef, 0x01, 0xc8, 0x27,
-	0x38, 0xc8, 0x58, 0x8e, 0x5c, 0xc9, 0xb6, 0xc4, 0x93, 0x2d, 0x52, 0xd1, 0x85, 0x85, 0xd9, 0x32,
-	0x3b, 0x92, 0xe6, 0x4b, 0x2c, 0x6b, 0x5c, 0xcf, 0x67, 0x2b, 0xff, 0xc6, 0xc2, 0x5a, 0x7e, 0x4b,
-	0x0a, 0xae, 0xe1, 0xa8, 0x2f, 0xfc, 0x40, 0xab, 0xaf, 0x87, 0xad, 0x3e, 0xdf, 0xd4, 0x37, 0x17,
-	0xdb, 0x6b, 0x54, 0x4b, 0xf6, 0xbd, 0x1e, 0x41, 0x32, 0x3c, 0x01, 0x8f, 0xa2, 0x14, 0xac, 0xc6,
-	0x2b, 0x54, 0xa9, 0x7d, 0x4a, 0x1e, 0x8c, 0x93, 0xd8, 0x4e, 0x6d, 0x42, 0x75, 0xb8, 0xf8, 0xeb,
-	0x00, 0xdc, 0x89, 0x90, 0x33, 0x38, 0x68, 0x49, 0xe4, 0x99, 0x75, 0x18, 0x6e, 0x56, 0xf0, 0xb0,
-	0x6f, 0x38, 0x22, 0xef, 0xb4, 0x8e, 0x61, 0x9e, 0x33, 0x46, 0x0e, 0x2d, 0x2c, 0x89, 0x83, 0x17,
-	0x1d, 0x61, 0x58, 0x4a, 0x38, 0xd2, 0x5e, 0xed, 0x22, 0x75, 0x5e, 0xc3, 0xbd, 0xda, 0xee, 0xf5,
-	0x06, 0xf6, 0x12, 0xfe, 0x55, 0x90, 0xa9, 0x05, 0x5c, 0xae, 0x0a, 0xd5, 0xdc, 0x47, 0x9b, 0xc7,
-	0x18, 0x8e, 0x6e, 0x9f, 0x98, 0x65, 0x7c, 0xff, 0x2f, 0x00, 0x00, 0xff, 0xff, 0xca, 0x64, 0x7d,
-	0x43, 0x5b, 0x04, 0x00, 0x00,
+	// 488 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x53, 0x5b, 0x6b, 0xdb, 0x30,
+	0x14, 0x8e, 0x9d, 0x76, 0x4d, 0x4e, 0x4a, 0x9a, 0x89, 0x5d, 0x82, 0x9f, 0x82, 0xe9, 0x43, 0xd9,
+	0xc5, 0x8c, 0x0c, 0x46, 0x19, 0x63, 0x50, 0xea, 0xc2, 0xfc, 0x50, 0x58, 0xd5, 0xd7, 0xbd, 0xb8,
+	0xce, 0xd9, 0x6a, 0xaa, 0x48, 0x9e, 0xa5, 0x18, 0xbc, 0xdf, 0x32, 0xf6, 0x3f, 0xf6, 0xef, 0x86,
+	0x2e, 0x4e, 0xed, 0xb4, 0xa1, 0x0c, 0xfa, 0x62, 0x1f, 0x1d, 0x7d, 0x97, 0xa3, 0x23, 0x1d, 0x98,
+	0x94, 0xc8, 0x17, 0xf8, 0xab, 0x12, 0x2b, 0x19, 0x15, 0xa5, 0x50, 0x82, 0xec, 0x48, 0xc1, 0x97,
+	0xc1, 0x41, 0xce, 0xf5, 0x9f, 0xe7, 0xa9, 0x4d, 0x07, 0xa3, 0x12, 0x59, 0x5a, 0xbb, 0xc5, 0x90,
+	0xa3, 0xb2, 0x61, 0xc8, 0x60, 0x7c, 0x2a, 0x38, 0xc7, 0x4c, 0x51, 0xfc, 0xb9, 0x42, 0xa9, 0xc8,
+	0x18, 0xfc, 0x24, 0x9e, 0x7a, 0x33, 0xef, 0x68, 0x48, 0xfd, 0x24, 0x26, 0x01, 0x0c, 0x0c, 0x34,
+	0x13, 0x6c, 0xea, 0x9b, 0xec, 0x7a, 0x4d, 0x22, 0xd8, 0x2f, 0xca, 0xbc, 0x4a, 0x15, 0x9e, 0x2c,
+	0x16, 0xa5, 0x9c, 0xf6, 0x67, 0xfd, 0xa3, 0xd1, 0x1c, 0x22, 0xed, 0x1d, 0xe9, 0x14, 0xed, 0xec,
+	0x87, 0xdf, 0x60, 0xfc, 0x75, 0x75, 0xc5, 0x72, 0x79, 0xdd, 0xb8, 0xb5, 0xd5, 0xbd, 0x07, 0xd4,
+	0xfd, 0x07, 0xd4, 0x97, 0x70, 0x40, 0xd7, 0xed, 0xa0, 0x58, 0xb0, 0x9a, 0xbc, 0x02, 0x28, 0xb4,
+	0x61, 0xa6, 0x11, 0xc6, 0xa0, 0x2b, 0xd0, 0xda, 0xfd, 0x6f, 0xbb, 0xdf, 0x5e, 0xdb, 0xef, 0x52,
+	0xa5, 0x0a, 0xc9, 0x07, 0xd8, 0x95, 0x3a, 0x98, 0x7a, 0x86, 0x3c, 0xb3, 0xe4, 0x0d, 0x54, 0x64,
+	0xbe, 0x67, 0x5c, 0x95, 0x35, 0xb5, 0xf0, 0xe0, 0x02, 0xe0, 0x36, 0x49, 0x26, 0xd0, 0xbf, 0xc1,
+	0xda, 0xf5, 0x43, 0x87, 0xe4, 0x2d, 0xec, 0x56, 0x29, 0x5b, 0xa1, 0xb9, 0x81, 0xd1, 0xfc, 0xe5,
+	0xa6, 0xee, 0x39, 0xa2, 0xca, 0xf9, 0x0f, 0x6a, 0x51, 0x1f, 0xfd, 0x63, 0x2f, 0xfc, 0xeb, 0xc3,
+	0xd3, 0x3b, 0x00, 0xf2, 0x19, 0xf6, 0x32, 0x96, 0x23, 0x57, 0xd2, 0x95, 0x78, 0xb8, 0x45, 0x2a,
+	0x3a, 0xb5, 0x30, 0x5b, 0x66, 0x43, 0xd2, 0x7c, 0x89, 0x65, 0x85, 0xeb, 0xfe, 0x6c, 0xe5, 0x5f,
+	0x5a, 0x98, 0xe3, 0x3b, 0x52, 0x70, 0x01, 0xfb, 0x6d, 0xe1, 0x7b, 0x8e, 0xfa, 0xba, 0x7b, 0xd4,
+	0xe7, 0x9b, 0xfa, 0xe6, 0x62, 0x5b, 0x07, 0xd5, 0x92, 0x6d, 0xaf, 0x47, 0x90, 0x0c, 0x0f, 0x61,
+	0x42, 0x51, 0x0a, 0x56, 0xe1, 0x39, 0xaa, 0xd4, 0x3e, 0xa5, 0x09, 0xf4, 0x93, 0xd8, 0x76, 0x6d,
+	0x48, 0x75, 0x38, 0xff, 0xe3, 0x03, 0xdc, 0x8a, 0x90, 0x63, 0xd8, 0x73, 0x24, 0xf2, 0xcc, 0x3a,
+	0x74, 0x27, 0x2b, 0xb8, 0xdf, 0x37, 0xec, 0x91, 0x77, 0x5a, 0xc7, 0x30, 0x4f, 0x18, 0x23, 0x03,
+	0x0b, 0x4b, 0xe2, 0xe0, 0x45, 0x43, 0xe8, 0x96, 0x12, 0xf6, 0xb4, 0x97, 0x1b, 0xa4, 0xc6, 0xab,
+	0x3b, 0x57, 0xdb, 0xbd, 0xde, 0xc0, 0x4e, 0xc2, 0xbf, 0x0b, 0x32, 0xb2, 0x80, 0xb3, 0x65, 0xa1,
+	0xea, 0xbb, 0x68, 0xf3, 0x18, 0xc3, 0x1e, 0xf9, 0x04, 0x83, 0x38, 0x97, 0x99, 0xa8, 0xb0, 0x24,
+	0xae, 0x9a, 0x2f, 0x29, 0x5f, 0xc8, 0xeb, 0xf4, 0x06, 0x1b, 0x2b, 0x97, 0x6f, 0x70, 0x14, 0x65,
+	0x21, 0xb8, 0xc4, 0xb0, 0x77, 0xf5, 0xc4, 0x8c, 0xf2, 0xfb, 0x7f, 0x01, 0x00, 0x00, 0xff, 0xff,
+	0x1d, 0xb8, 0xef, 0x42, 0xa6, 0x04, 0x00, 0x00,
 }

sonm-io/core

proto/rendezvous.pb.go
@@ -211,6 +211,7 @@ type RendezvousClient interface {
 	Publish(ctx context.Context, in *PublishRequest, opts ...grpc.CallOption) (*RendezvousReply, error)
 	// Info returns server's internal state.
 	Info(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*RendezvousState, error)
+	Discover(ctx context.Context, in *HandshakeRequest, opts ...grpc.CallOption) (*DiscoverResponse, error)
 }
 
 type rendezvousClient struct {
@@ -257,6 +258,15 @@ func (c *rendezvousClient) Info(ctx context.Context, in *Empty, opts ...grpc.Cal
 	return out, nil
 }
 
+func (c *rendezvousClient) Discover(ctx context.Context, in *HandshakeRequest, opts ...grpc.CallOption) (*DiscoverResponse, error) {
+	out := new(DiscoverResponse)
+	err := grpc.Invoke(ctx, "/sonm.Rendezvous/Discover", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // Server API for Rendezvous service
 
 type RendezvousServer interface {
@@ -275,6 +285,7 @@ type RendezvousServer interface {
 	Publish(context.Context, *PublishRequest) (*RendezvousReply, error)
 	// Info returns server's internal state.
 	Info(context.Context, *Empty) (*RendezvousState, error)
+	Discover(context.Context, *HandshakeRequest) (*DiscoverResponse, error)
 }
 
 func RegisterRendezvousServer(s *grpc.Server, srv RendezvousServer) {
@@ -353,6 +364,24 @@ func _Rendezvous_Info_Handler(srv interface{}, ctx context.Context, dec func(int
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Rendezvous_Discover_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(HandshakeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(RendezvousServer).Discover(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/sonm.Rendezvous/Discover",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(RendezvousServer).Discover(ctx, req.(*HandshakeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Rendezvous_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "sonm.Rendezvous",
 	HandlerType: (*RendezvousServer)(nil),
@@ -372,6 +401,10 @@ var _Rendezvous_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Info",
 			Handler:    _Rendezvous_Info_Handler,
+		},
+		{
+			MethodName: "Discover",
+			Handler:    _Rendezvous_Discover_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -463,6 +496,25 @@ var _Rendezvous_InfoCmd_gen = &cobra.Command{
 	RunE:  grpccmd.TypeToJson("sonm.Empty"),
 }
 
+var _Rendezvous_DiscoverCmd = &cobra.Command{
+	Use:   "discover",
+	Short: "Make the Discover method call, input-type: sonm.HandshakeRequest output-type: sonm.DiscoverResponse",
+	RunE: grpccmd.RunE(
+		"Discover",
+		"sonm.HandshakeRequest",
+		func(c io.Closer) interface{} {
+			cc := c.(*grpc.ClientConn)
+			return NewRendezvousClient(cc)
+		},
+	),
+}
+
+var _Rendezvous_DiscoverCmd_gen = &cobra.Command{
+	Use:   "discover-gen",
+	Short: "Generate JSON for method call of Discover (input-type: sonm.HandshakeRequest)",
+	RunE:  grpccmd.TypeToJson("sonm.HandshakeRequest"),
+}
+
 // Register commands with the root command and service command
 func init() {
 	grpccmd.RegisterServiceCmd(_RendezvousCmd)
@@ -475,6 +527,8 @@ func init() {
 		_Rendezvous_PublishCmd_gen,
 		_Rendezvous_InfoCmd,
 		_Rendezvous_InfoCmd_gen,
+		_Rendezvous_DiscoverCmd,
+		_Rendezvous_DiscoverCmd_gen,
 	)
 }
 
@@ -483,34 +537,36 @@ func init() {
 func init() { proto.RegisterFile("rendezvous.proto", fileDescriptor11) }
 
 var fileDescriptor11 = []byte{
-	// 456 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x53, 0xcd, 0x6a, 0xdb, 0x40,
-	0x10, 0xf6, 0xca, 0x49, 0x13, 0x8f, 0x8d, 0xe3, 0x2e, 0xfd, 0x31, 0x3a, 0x19, 0x91, 0x83, 0xe9,
-	0x8f, 0x28, 0x2e, 0x94, 0xd0, 0x43, 0x21, 0xc4, 0x39, 0xe8, 0x10, 0x68, 0x36, 0xd7, 0x5e, 0x14,
-	0x79, 0xda, 0x8a, 0xae, 0x77, 0x55, 0xed, 0x4a, 0xa0, 0x3e, 0x4b, 0x5f, 0xa4, 0x6f, 0xd3, 0x47,
-	0x29, 0xbb, 0x2b, 0x39, 0x92, 0x13, 0x63, 0x0a, 0xb9, 0xd8, 0xa3, 0xd9, 0xef, 0x67, 0x66, 0x76,
-	0x07, 0x26, 0x39, 0x8a, 0x15, 0xfe, 0x2a, 0x65, 0xa1, 0xc2, 0x2c, 0x97, 0x5a, 0xd2, 0x03, 0x25,
-	0xc5, 0xda, 0x3f, 0x49, 0x85, 0xf9, 0x17, 0x69, 0xec, 0xd2, 0xfe, 0x40, 0xa0, 0x76, 0x61, 0xc0,
-	0x61, 0x7c, 0x21, 0x85, 0xc0, 0x44, 0x33, 0xfc, 0x59, 0xa0, 0xd2, 0x74, 0x0c, 0x5e, 0xb4, 0x9c,
-	0x92, 0x19, 0x99, 0x8f, 0x98, 0x17, 0x2d, 0xa9, 0x0f, 0xc7, 0x16, 0x9a, 0x48, 0x3e, 0xf5, 0x66,
-	0x64, 0x3e, 0x60, 0x9b, 0x6f, 0x1a, 0xc2, 0x28, 0xcb, 0xd3, 0x32, 0xd6, 0x78, 0xbe, 0x5a, 0xe5,
-	0x6a, 0xda, 0x9f, 0xf5, 0xe7, 0xc3, 0x05, 0x84, 0xc6, 0x2e, 0x34, 0x29, 0xd6, 0x39, 0x0f, 0xbe,
-	0xc0, 0xf8, 0x73, 0x71, 0xcb, 0x53, 0xf5, 0xbd, 0x71, 0x6b, 0xab, 0x93, 0x3d, 0xea, 0xde, 0x1e,
-	0xf5, 0x35, 0x9c, 0xb0, 0xcd, 0x04, 0x18, 0x66, 0xbc, 0xa2, 0xaf, 0x00, 0x32, 0x63, 0x98, 0x18,
-	0x84, 0x35, 0xe8, 0x0a, 0xb4, 0x4e, 0xff, 0xdb, 0xee, 0x37, 0x69, 0xfb, 0xdd, 0xe8, 0x58, 0x23,
-	0xfd, 0x00, 0x87, 0xca, 0x04, 0x53, 0x62, 0xc9, 0x33, 0x47, 0xde, 0x42, 0x85, 0xf6, 0xf7, 0x52,
-	0xe8, 0xbc, 0x62, 0x0e, 0xee, 0x5f, 0x03, 0xdc, 0x25, 0xe9, 0x04, 0xfa, 0x3f, 0xb0, 0xaa, 0xe7,
-	0x61, 0x42, 0xfa, 0x16, 0x0e, 0xcb, 0x98, 0x17, 0x68, 0x6f, 0x60, 0xb8, 0x78, 0xb9, 0xad, 0x7b,
-	0x85, 0xa8, 0x53, 0xf1, 0x8d, 0x39, 0xd4, 0x47, 0xef, 0x8c, 0x04, 0x7f, 0x3c, 0x78, 0x7a, 0x0f,
-	0x40, 0x3f, 0xc1, 0x51, 0xc2, 0x53, 0x14, 0x5a, 0xd5, 0x25, 0x9e, 0xee, 0x90, 0x0a, 0x2f, 0x1c,
-	0xcc, 0x95, 0xd9, 0x90, 0x0c, 0x5f, 0x61, 0x5e, 0xe2, 0x66, 0x3e, 0x3b, 0xf9, 0x37, 0x0e, 0x56,
-	0xf3, 0x6b, 0x92, 0x7f, 0x0d, 0xa3, 0xb6, 0xf0, 0x03, 0xad, 0xbe, 0xee, 0xb6, 0xfa, 0x7c, 0x5b,
-	0xdf, 0x5e, 0x6c, 0xab, 0x51, 0x23, 0xd9, 0xf6, 0x7a, 0x04, 0xc9, 0xe0, 0x14, 0x26, 0x0c, 0x95,
-	0xe4, 0x25, 0x5e, 0xa1, 0x8e, 0xdd, 0x53, 0x9a, 0x40, 0x3f, 0x5a, 0xba, 0xa9, 0x0d, 0x98, 0x09,
-	0x17, 0x7f, 0x09, 0xc0, 0x9d, 0x08, 0x3d, 0x83, 0xa3, 0x9a, 0x44, 0x9f, 0x39, 0x87, 0xee, 0x66,
-	0xf9, 0x0f, 0xfb, 0x06, 0x3d, 0xfa, 0xce, 0xe8, 0x58, 0xe6, 0x39, 0xe7, 0xf4, 0xd8, 0xc1, 0xa2,
-	0xa5, 0xff, 0xa2, 0x21, 0x74, 0x4b, 0x09, 0x7a, 0xc6, 0xab, 0x5e, 0xa4, 0xc6, 0xab, 0xbb, 0x57,
-	0xbb, 0xbd, 0xde, 0xc0, 0x41, 0x24, 0xbe, 0x4a, 0x3a, 0x74, 0x80, 0xcb, 0x75, 0xa6, 0xab, 0xfb,
-	0x68, 0xfb, 0x18, 0x83, 0xde, 0xed, 0x13, 0xbb, 0x8c, 0xef, 0xff, 0x05, 0x00, 0x00, 0xff, 0xff,
-	0xeb, 0x0f, 0xce, 0x01, 0x5b, 0x04, 0x00, 0x00,
+	// 490 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x53, 0x5b, 0x6b, 0xdb, 0x30,
+	0x14, 0x8e, 0x9d, 0x76, 0x4d, 0x4e, 0x42, 0x9a, 0x89, 0x5d, 0x82, 0x9f, 0x82, 0xe9, 0x43, 0xd8,
+	0xc5, 0x8c, 0x0c, 0x46, 0x19, 0x63, 0x50, 0x9a, 0xc2, 0xfc, 0x50, 0x58, 0xd5, 0xd7, 0xbd, 0xb8,
+	0xce, 0xd9, 0x6a, 0xaa, 0x48, 0x9e, 0xa5, 0x18, 0xbc, 0xdf, 0x32, 0xf6, 0x3f, 0xf6, 0xef, 0x86,
+	0x2e, 0x4e, 0xed, 0xb4, 0xa1, 0x0c, 0xfa, 0x62, 0x1f, 0x1d, 0x7d, 0x97, 0xa3, 0x23, 0x1d, 0x18,
+	0x17, 0xc8, 0x97, 0xf8, 0xab, 0x14, 0x6b, 0x19, 0xe5, 0x85, 0x50, 0x82, 0xec, 0x49, 0xc1, 0x57,
+	0xc1, 0x61, 0xc6, 0xf5, 0x9f, 0x67, 0x89, 0x4d, 0x07, 0x83, 0x02, 0x59, 0x52, 0xb9, 0x45, 0x9f,
+	0xa3, 0xb2, 0x61, 0xc8, 0x60, 0x74, 0x2a, 0x38, 0xc7, 0x54, 0x51, 0xfc, 0xb9, 0x46, 0xa9, 0xc8,
+	0x08, 0xfc, 0x78, 0x31, 0xf1, 0xa6, 0xde, 0x6c, 0x48, 0xfd, 0x78, 0x41, 0x02, 0xe8, 0x19, 0x68,
+	0x2a, 0xd8, 0xc4, 0x9f, 0x7a, 0xb3, 0x3e, 0xdd, 0xac, 0x49, 0x04, 0xc3, 0xbc, 0xc8, 0xca, 0x44,
+	0xe1, 0xc9, 0x72, 0x59, 0xc8, 0x49, 0x77, 0xda, 0x9d, 0x0d, 0xe6, 0x10, 0x69, 0xef, 0x48, 0xa7,
+	0x68, 0x6b, 0x3f, 0xfc, 0x06, 0xa3, 0xaf, 0xeb, 0x2b, 0x96, 0xc9, 0xeb, 0xda, 0xad, 0xa9, 0xee,
+	0x3d, 0xa0, 0xee, 0x3f, 0xa0, 0xbe, 0x82, 0x43, 0xba, 0x69, 0x07, 0xc5, 0x9c, 0x55, 0xe4, 0x15,
+	0x40, 0xae, 0x0d, 0x53, 0x8d, 0x30, 0x06, 0x6d, 0x81, 0xc6, 0xee, 0x7f, 0xdb, 0xfd, 0xf6, 0x9a,
+	0x7e, 0x97, 0x2a, 0x51, 0x48, 0x3e, 0xc0, 0xbe, 0xd4, 0xc1, 0xc4, 0x33, 0xe4, 0xa9, 0x25, 0x6f,
+	0xa1, 0x22, 0xf3, 0x3d, 0xe3, 0xaa, 0xa8, 0xa8, 0x85, 0x07, 0x17, 0x00, 0xb7, 0x49, 0x32, 0x86,
+	0xee, 0x0d, 0x56, 0xae, 0x1f, 0x3a, 0x24, 0x6f, 0x61, 0xbf, 0x4c, 0xd8, 0x1a, 0xcd, 0x0d, 0x0c,
+	0xe6, 0x2f, 0xb7, 0x75, 0xcf, 0x11, 0x55, 0xc6, 0x7f, 0x50, 0x8b, 0xfa, 0xe8, 0x1f, 0x7b, 0xe1,
+	0x5f, 0x1f, 0x9e, 0xde, 0x01, 0x90, 0xcf, 0x70, 0x90, 0xb2, 0x0c, 0xb9, 0x92, 0xae, 0xc4, 0xa3,
+	0x1d, 0x52, 0xd1, 0xa9, 0x85, 0xd9, 0x32, 0x6b, 0x92, 0xe6, 0x4b, 0x2c, 0x4a, 0xdc, 0xf4, 0x67,
+	0x27, 0xff, 0xd2, 0xc2, 0x1c, 0xdf, 0x91, 0x82, 0x0b, 0x18, 0x36, 0x85, 0xef, 0x39, 0xea, 0xeb,
+	0xf6, 0x51, 0x9f, 0x6f, 0xeb, 0x9b, 0x8b, 0x6d, 0x1c, 0x54, 0x4b, 0x36, 0xbd, 0x1e, 0x41, 0x32,
+	0x3c, 0x82, 0x31, 0x45, 0x29, 0x58, 0x89, 0xe7, 0xa8, 0x12, 0xfb, 0x94, 0xc6, 0xd0, 0x8d, 0x17,
+	0xb6, 0x6b, 0x7d, 0xaa, 0xc3, 0xf9, 0x1f, 0x1f, 0xe0, 0x56, 0x84, 0x1c, 0xc3, 0x81, 0x23, 0x91,
+	0x67, 0xd6, 0xa1, 0x3d, 0x59, 0xc1, 0xfd, 0xbe, 0x61, 0x87, 0xbc, 0xd3, 0x3a, 0x86, 0x79, 0xc2,
+	0x18, 0xe9, 0x59, 0x58, 0xbc, 0x08, 0x5e, 0xd4, 0x84, 0x76, 0x29, 0x61, 0x47, 0x7b, 0xb9, 0x41,
+	0xaa, 0xbd, 0xda, 0x73, 0xb5, 0xdb, 0xeb, 0x0d, 0xec, 0xc5, 0xfc, 0xbb, 0x20, 0x03, 0x0b, 0x38,
+	0x5b, 0xe5, 0xaa, 0xba, 0x8b, 0x36, 0x8f, 0x31, 0xec, 0x90, 0x4f, 0xd0, 0x5b, 0x64, 0x32, 0x15,
+	0x25, 0x16, 0xc4, 0x55, 0xf3, 0x25, 0xe1, 0x4b, 0x79, 0x9d, 0xdc, 0x60, 0x6d, 0xe5, 0xf2, 0x35,
+	0x8e, 0xa2, 0xcc, 0x05, 0x97, 0x18, 0x76, 0xae, 0x9e, 0x98, 0x51, 0x7e, 0xff, 0x2f, 0x00, 0x00,
+	0xff, 0xff, 0xc0, 0x7d, 0x2a, 0xbe, 0xa6, 0x04, 0x00, 0x00,
 }