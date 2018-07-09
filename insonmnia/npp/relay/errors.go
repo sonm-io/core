sonm-io/core

commit 1b71152af1d8dac33c4e991e259e877fd31e985e

fix: relay check matching node (#1046)
It might be the case that cluster reconfigures between Discover and Handshake; this PR checks that we only work with appropriate peers. It also adds dial retries for clients that experienced Discover/Handshake inconsistency.

insonmnia/npp/relay/errors.go
@@ -11,6 +11,7 @@ const (
 	ErrUnknownPeerType
 	ErrTimeout
 	ErrNoPeer
+	ErrWrongNode
 )
 
 type protocolError struct {
@@ -43,4 +44,8 @@ func errTimeout() error {
 
 func errNoPeer() error {
 	return newProtocolError(ErrNoPeer, fmt.Errorf("no peer found"))
+}
+
+func errWrongNode() error {
+	return newProtocolError(ErrWrongNode, fmt.Errorf("peer connected to wrong node"))
 }

insonmnia/npp/relay/server.go
@@ -60,6 +60,7 @@ package relay
 import (
 	"context"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 	"math/rand"
@@ -499,6 +500,16 @@ func (m *server) processHandshake(ctx context.Context, conn net.Conn, handshake 
 	addr := nppc.ResourceID{
 		Protocol: handshake.Protocol,
 		Addr:     common.BytesToAddress(handshake.Addr),
+	}
+
+	targetAddr, ok := m.continuum.Get(addr)
+	if !ok {
+		return errInvalidHandshake(errors.New("failed to get node by address"))
+	}
+
+	// Peer might have got a no longer valid node address while discovery.
+	if targetAddr != m.cfg.Addr.String() {
+		return errWrongNode()
 	}
 
 	// We support both multiple servers and clients.

insonmnia/npp/relay/client.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/gogo/protobuf/proto"
+	"github.com/pkg/errors"
 	"github.com/sonm-io/core/proto"
 	"github.com/sonm-io/core/util/multierror"
 	"github.com/sonm-io/core/util/netutil"
@@ -40,20 +41,28 @@ func DialWithLog(addr net.Addr, targetAddr common.Address, uuid string, log *zap
 	log = log.With(zap.Stringer("addr", targetAddr))
 	log.Debug("discovering meeting point on the Continuum")
 
-	member, err := client.discover(targetAddr)
-	if err != nil {
-		log.Warn("failed to discover meeting point on the Continuum", zap.Error(err))
-		return nil, err
+	for numAttempts := 0; numAttempts < 2; numAttempts++ {
+		member, err := client.discover(targetAddr)
+		if err != nil {
+			log.Warn("failed to discover meeting point on the Continuum", zap.Error(err))
+			return nil, err
+		}
+
+		log.Debug("connecting to remote meeting point on the Continuum", zap.Stringer("remote_addr", member.conn.RemoteAddr()))
+		conn, err := member.dial(targetAddr, uuid)
+		if err == nil {
+			return conn, nil
+		}
+
+		if verboseErr, ok := err.(*protocolError); ok && verboseErr.code == ErrWrongNode {
+			continue
+		} else {
+			log.Warn("failed to connect to remote meeting point on the Continuum", zap.Error(err))
+			return nil, err
+		}
 	}
 
-	log.Debug("connecting to remote meeting point on the Continuum", zap.Stringer("remote_addr", member.conn.RemoteAddr()))
-	conn, err := member.dial(targetAddr, uuid)
-	if err != nil {
-		log.Warn("failed to connect to remote meeting point on the Continuum", zap.Error(err))
-		return nil, err
-	}
-
-	return conn, err
+	return nil, errors.New("failed to dial remote")
 }
 
 // Listen publishes itself to the relay server waiting for other client peer

commit a0f150cdb48d35892661d27fd1ef0fe20d9691d2

fix: spurious continuum devastation
This fixes a bug in the Relay server, when it suddenly devastates the continuum, returning 0.0.0.0 on any discovery request.

Also it fixes a bug introduced in #1046, when an inproperly configured node stopped to work.

insonmnia/npp/relay/errors.go
@@ -12,6 +12,7 @@ const (
 	ErrTimeout
 	ErrNoPeer
 	ErrWrongNode
+	ErrEmptyContinuum
 )
 
 type protocolError struct {
@@ -48,4 +49,8 @@ func errNoPeer() error {
 
 func errWrongNode() error {
 	return newProtocolError(ErrWrongNode, fmt.Errorf("peer connected to wrong node"))
+}
+
+func errEmptyContinuum() error {
+	return newProtocolError(ErrEmptyContinuum, fmt.Errorf("no nodes in the continuum"))
 }

insonmnia/npp/relay/hashring.go
@@ -21,6 +21,8 @@
 package relay
 
 import (
+	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/serialx/hashring"
@@ -101,9 +103,49 @@ func (m *continuum) scanTrackingChanges() []nppc.ResourceID {
 	return addrs
 }
 
-// Get returns a node that will serve the specified ETH address.
-func (m *continuum) Get(addr nppc.ResourceID) (string, bool) {
+func (m *continuum) get(addr nppc.ResourceID) (string, bool) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	return m.continuum.GetNode(addr.String())
+}
+
+// GetNode returns a node that will serve the specified ETH address.
+func (m *continuum) GetNode(addr nppc.ResourceID) (*Node, error) {
+	node, ok := m.get(addr)
+	if !ok {
+		return nil, errEmptyContinuum()
+	}
+
+	return ParseNode(node)
+}
+
+// Node represents a node point on the Continuum.
+type Node struct {
+	Name string
+	Addr string
+}
+
+func newNode(name, addr string) *Node {
+	return &Node{
+		Name: name,
+		Addr: addr,
+	}
+}
+
+func ParseNode(node string) (*Node, error) {
+	parts := strings.SplitN(node, "@", 2)
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("continuum node must be in `<name>@<addr>` format")
+	}
+
+	m := &Node{
+		Name: parts[0],
+		Addr: parts[1],
+	}
+
+	return m, nil
+}
+
+func (m *Node) String() string {
+	return fmt.Sprintf("%s@%s", m.Name, m.Addr)
 }

insonmnia/npp/relay/server.go
@@ -60,7 +60,6 @@ package relay
 import (
 	"context"
 	"encoding/hex"
-	"errors"
 	"fmt"
 	"io"
 	"math/rand"
@@ -480,13 +479,13 @@ func (m *server) processConnectionBlocking(ctx context.Context, conn net.Conn) e
 func (m *server) processDiscover(ctx context.Context, conn net.Conn, addr nppc.ResourceID) error {
 	m.log.Debugf("processing discover request %s", conn.RemoteAddr())
 
-	targetAddr, ok := m.continuum.Get(addr)
-	if !ok {
-		targetAddr = m.cfg.Addr.String()
+	targetNode, err := m.continuum.GetNode(addr)
+	if err != nil {
+		return err
 	}
 
-	m.log.Debugf("redirecting handshake for %s to %s", conn.RemoteAddr(), targetAddr)
-	return sendFrame(conn, newDiscoverResponse(targetAddr))
+	m.log.Debugf("redirecting handshake for %s to %s", conn.RemoteAddr(), targetNode.String())
+	return sendFrame(conn, newDiscoverResponse(targetNode.Addr))
 }
 
 func (m *server) processHandshake(ctx context.Context, conn net.Conn, handshake *sonm.HandshakeRequest) error {
@@ -502,13 +501,13 @@ func (m *server) processHandshake(ctx context.Context, conn net.Conn, handshake 
 		Addr:     common.BytesToAddress(handshake.Addr),
 	}
 
-	targetAddr, ok := m.continuum.Get(addr)
-	if !ok {
-		return errInvalidHandshake(errors.New("failed to get node by address"))
+	targetNode, err := m.continuum.GetNode(addr)
+	if err != nil {
+		return err
 	}
 
 	// Peer might have got a no longer valid node address while discovery.
-	if targetAddr != m.cfg.Addr.String() {
+	if targetNode.Name != m.cfg.Cluster.Name {
 		return errWrongNode()
 	}
 
@@ -604,19 +603,23 @@ func (m *server) Close() error {
 func (m *server) NotifyJoin(node *memberlist.Node) {
 	m.log.Infof("node `%s` has joined to the cluster from %s", node.Name, node.Address())
 
-	discarded := m.continuum.Add(m.formatEndpoint(node.Addr), 1)
+	discarded := m.continuum.Add(m.formatNode(node), 1)
 	m.meetingRoom.DiscardConnections(discarded)
 }
 
 func (m *server) NotifyLeave(node *memberlist.Node) {
 	m.log.Infof("node `%s` has left from the cluster from %s", node.Name, node.Address())
 
-	discarded := m.continuum.Remove(m.formatEndpoint(node.Addr))
+	discarded := m.continuum.Remove(m.formatNode(node))
 	m.meetingRoom.DiscardConnections(discarded)
 }
 
 func (m *server) NotifyUpdate(node *memberlist.Node) {
 	m.log.Infof("node `%s` has been updated", node.Name)
+}
+
+func (m *server) formatNode(node *memberlist.Node) string {
+	return newNode(node.Name, m.formatEndpoint(node.Addr)).String()
 }
 
 func (m *server) formatEndpoint(ip net.IP) string {