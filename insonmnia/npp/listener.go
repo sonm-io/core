sonm-io/core

commit 1ca8b883262d73afe95d9d796eb0bc51344f335a

refactor: properly use context in NPP (#1189)
Suddenly we discovered that there is no need for passing context
variables into NPP dialer and listener's construction functions.

insonmnia/npp/listener.go
@@ -92,7 +92,7 @@ type Listener struct {
 	listenerChannel chan connTuple
 
 	puncher    NATPuncher
-	puncherNew func() (NATPuncher, error)
+	puncherNew func(ctx context.Context) (NATPuncher, error)
 	nppChannel chan connTuple
 
 	relayListener *relay.Listener
@@ -106,7 +106,7 @@ type Listener struct {
 // network address with TCP protocol, switching to the NPP when there is no
 // pending connections available.
 func NewListener(ctx context.Context, addr string, options ...Option) (*Listener, error) {
-	opts := newOptions(ctx)
+	opts := newOptions()
 
 	for _, o := range options {
 		if err := o(opts); err != nil {
@@ -122,7 +122,6 @@ func NewListener(ctx context.Context, addr string, options ...Option) (*Listener
 	}
 
 	m := &Listener{
-		ctx:             ctx,
 		metrics:         newMetrics(),
 		log:             opts.log,
 		listenerChannel: channel,
@@ -182,7 +181,7 @@ func (m *Listener) listenPuncher(ctx context.Context) error {
 
 		if m.puncher == nil {
 			m.log.Debug("constructing new puncher")
-			puncher, err := m.puncherNew()
+			puncher, err := m.puncherNew(ctx)
 			if err != nil {
 				m.log.Warn("failed to construct a puncher", zap.Error(err))
 				if timeout < m.maxBackoffInterval {

insonmnia/node/server.go
@@ -79,7 +79,7 @@ func newRemoteOptions(ctx context.Context, key *ecdsa.PrivateKey, cfg *Config, c
 		npp.WithRelayDialer(&relay.Dialer{Addrs: cfg.NPP.Relay.Endpoints, Log: log.G(ctx)}),
 		npp.WithLogger(log.G(ctx)),
 	}
-	nppDialer, err := npp.NewDialer(ctx, nppDialerOptions...)
+	nppDialer, err := npp.NewDialer(nppDialerOptions...)
 	if err != nil {
 		return nil, err
 	}

insonmnia/npp/dial.go
@@ -18,16 +18,15 @@ import (
 // must be an authenticated endpoint and the connection establishment process
 // is done via NAT Punching Protocol.
 type Dialer struct {
-	ctx context.Context
 	log *zap.Logger
 
-	puncherNew  func() (NATPuncher, error)
+	puncherNew  func(ctx context.Context) (NATPuncher, error)
 	relayDialer *relay.Dialer
 }
 
 // NewDialer constructs a new dialer that is aware of NAT Punching Protocol.
-func NewDialer(ctx context.Context, options ...Option) (*Dialer, error) {
-	opts := newOptions(ctx)
+func NewDialer(options ...Option) (*Dialer, error) {
+	opts := newOptions()
 
 	for _, o := range options {
 		if err := o(opts); err != nil {
@@ -36,7 +35,6 @@ func NewDialer(ctx context.Context, options ...Option) (*Dialer, error) {
 	}
 
 	return &Dialer{
-		ctx:         ctx,
 		log:         opts.log,
 		puncherNew:  opts.puncherNew,
 		relayDialer: opts.relayDialer,
@@ -78,7 +76,7 @@ func (m *Dialer) DialContext(ctx context.Context, addr auth.Addr) (net.Conn, err
 		nppChannel := make(chan connTuple)
 
 		go func() {
-			puncher, err := m.puncherNew()
+			puncher, err := m.puncherNew(nppCtx)
 			if err != nil {
 				nppChannel <- newConnTuple(nil, err)
 				return

insonmnia/npp/options.go
@@ -15,10 +15,9 @@ import (
 type Option func(o *options) error
 
 type options struct {
-	ctx                   context.Context
 	log                   *zap.Logger
 	puncher               NATPuncher
-	puncherNew            func() (NATPuncher, error)
+	puncherNew            func(ctx context.Context) (NATPuncher, error)
 	nppBacklog            int
 	nppMinBackoffInterval time.Duration
 	nppMaxBackoffInterval time.Duration
@@ -26,9 +25,8 @@ type options struct {
 	relayDialer           *relay.Dialer
 }
 
-func newOptions(ctx context.Context) *options {
+func newOptions() *options {
 	return &options{
-		ctx:                   ctx,
 		log:                   zap.NewNop(),
 		nppBacklog:            128,
 		nppMinBackoffInterval: 500 * time.Millisecond,
@@ -47,11 +45,11 @@ func WithRendezvous(cfg rendezvous.Config, credentials credentials.TransportCred
 			return nil
 		}
 
-		o.puncherNew = func() (NATPuncher, error) {
+		o.puncherNew = func(ctx context.Context) (NATPuncher, error) {
 			for _, addr := range cfg.Endpoints {
-				client, err := newRendezvousClient(o.ctx, addr, credentials)
+				client, err := newRendezvousClient(ctx, addr, credentials)
 				if err == nil {
-					return newNATPuncher(o.ctx, cfg, client)
+					return newNATPuncher(ctx, cfg, client)
 				}
 			}
 

commit 6daf412fc999762ac35c2d3dce27f51c7610e7bd

fix: using nil variable
Fixes bug introduced in #1189.

insonmnia/npp/listener.go
@@ -84,7 +84,6 @@ func (m *connTuple) unwrapWithSource(source connSource) (net.Conn, connSource, e
 //
 // Options are: rendezvous server, private IPs usage, relay server(s) if any.
 type Listener struct {
-	ctx     context.Context
 	metrics *metrics
 	log     *zap.Logger
 
@@ -246,7 +245,11 @@ func (m *Listener) listenRelay(ctx context.Context) error {
 // punching mechanism work. This can consume a meaningful amount of file
 // descriptors, so be prepared to enlarge your limits.
 func (m *Listener) Accept() (net.Conn, error) {
-	conn, source, err := m.accept()
+	return m.AcceptContext(context.Background())
+}
+
+func (m *Listener) AcceptContext(ctx context.Context) (net.Conn, error) {
+	conn, source, err := m.accept(ctx)
 	if err != nil {
 		m.log.Warn("failed to accept peer", zap.Error(err))
 		return nil, err
@@ -270,7 +273,7 @@ func (m *Listener) Accept() (net.Conn, error) {
 
 // Note: this function only listens for multiple channels and transforms the
 // result from a single-value to multiple values, due to weird Go type system.
-func (m *Listener) accept() (net.Conn, connSource, error) {
+func (m *Listener) accept(ctx context.Context) (net.Conn, connSource, error) {
 	// Act as a listener if there is no puncher specified.
 	// Check for acceptor listenerChannel, if there is a connection - return immediately.
 	select {
@@ -282,8 +285,8 @@ func (m *Listener) accept() (net.Conn, connSource, error) {
 	// Otherwise block when either a new connection arrives or NPP does its job.
 	for {
 		select {
-		case <-m.ctx.Done():
-			return nil, sourceError, m.ctx.Err()
+		case <-ctx.Done():
+			return nil, sourceError, ctx.Err()
 		case conn := <-m.listenerChannel:
 			return conn.unwrapWithSource(sourceDirectConnection)
 		case conn := <-m.nppChannel: