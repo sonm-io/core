7551ef36e653f217f9fd7a73037e4a9115dabdc3
Evgeny Safronov <division494@gmail.com>
2018-07-27 13:28:35 +0300

    feat: genetic optimization in Optimus (#1234)
    This commit introduces a new genetic optimization algorithm helping
    Optimus to find the best orders set.
    
    The core idea is to create multiple knapsack populations with random
    orders inside and try to pack them. Failed populations should die, while
    successful ones will live. Sometimes random mutation occurs, which
    results in random order addition or removal from the set. The best
    individuals will cross each other attempting to produce more adapted
    children.
    
    This is quite CPU intensive algorithm, so there are predefined
    generation and age limits.
    
    Additionally now it is possible to combine several models to run them
    parallel and to select the best one.

--- optimus/engine.go
+++ optimus/engine.go
@@ -3,7 +3,6 @@ package optimus
 import (
 	"context"
 	"fmt"
-	"math"
 	"math/big"
 	"sync"
 	"time"
@@ -498,92 +497,35 @@ func (m *workerEngine) filters(deviceManager *DeviceManager, devices *sonm.Devic
 }
 
 func (m *workerEngine) optimizationMethod(orders []*MarketOrder, log *zap.SugaredLogger) OptimizationMethod {
-	method := &GreedyLinearRegressionModel{
-		orders:          orders,
-		classifier:      m.optimizationConfig.ClassifierFactory(m.log.Desugar()),
-		exhaustionLimit: 128,
-		log:             log,
+	return &BatchModel{
+		Methods: []OptimizationMethod{
+			&GreedyLinearRegressionModel{
+				orders:          orders,
+				classifier:      m.optimizationConfig.ClassifierFactory(m.log.Desugar()),
+				exhaustionLimit: 128,
+				log:             log.With(zap.String("model", "lls")),
+			},
+			&GeneticModel{
+				NewGenomeLab:   NewPackedOrdersNewGenome,
+				PopulationSize: 256,
+				MaxGenerations: 128,
+				MaxAge:         5 * time.Minute,
+				Log:            log.With(zap.String("model", "gmp")),
+			},
+			&GeneticModel{
+				NewGenomeLab:   NewDecisionOrdersNewGenome,
+				PopulationSize: 512,
+				MaxGenerations: 64,
+				MaxAge:         5 * time.Minute,
+				Log:            log.With(zap.String("model", "gmd")),
+			},
+		},
+		Log: log,
 	}
-
-	return method
 }
 
 type OptimizationMethod interface {
 	Optimize(knapsack *Knapsack, orders []*MarketOrder) error
-}
-
-type BruteForceModel struct{}
-
-// GreedyLinearRegressionModel implements greedy knapsack optimization
-// algorithm.
-// The basic idea is to train the model using BID orders from the marketplace
-// by optimizing multidimensional linear regression over order benchmarks to
-// reduce the number of parameters to a single one - predicted price. This
-// price can be used to assign weights to orders to be able to determine which
-// orders are better to buy than others.
-type GreedyLinearRegressionModel struct {
-	orders          []*MarketOrder
-	classifier      OrderClassifier
-	exhaustionLimit int
-	log             *zap.SugaredLogger
-}
-
-func (m *GreedyLinearRegressionModel) Optimize(knapsack *Knapsack, orders []*MarketOrder) error {
-	if len(m.orders) <= minNumOrders {
-		return fmt.Errorf("not enough orders to perform optimization")
-	}
-
-	weightedOrders, err := m.classifier.Classify(m.orders)
-	if err != nil {
-		return fmt.Errorf("failed to classify orders: %v", err)
-	}
-
-	// Here we create an index of matching orders to be able to filter
-	// the entire training set for only interesting features.
-	filter := map[string]struct{}{}
-	for _, order := range orders {
-		filter[order.GetOrder().GetId().Unwrap().String()] = struct{}{}
-	}
-
-	exhaustedCounter := 0
-	for _, weightedOrder := range weightedOrders {
-		// Ignore orders with too low relative weight, i.e. orders that have
-		// quotient of its price to predicted price less than 1%.
-		// It may be, for example, when an order has 0 price.
-		// TODO: For now not sure where to perform this filtering. Let it be here.
-		if math.Abs(weightedOrder.Weight) < 0.01 {
-			m.log.Debugf("ignore `%s` order - weight too low: %.6f", weightedOrder.ID().String(), weightedOrder.Weight)
-			continue
-		}
-
-		if _, ok := filter[weightedOrder.ID().String()]; !ok {
-			continue
-		}
-
-		if exhaustedCounter >= m.exhaustionLimit {
-			break
-		}
-
-		order := weightedOrder.Order.Order
-
-		m.log.Debugw("trying to put an order into resources pool",
-			zap.Any("order", *weightedOrder.Order),
-			zap.Float64("weight", weightedOrder.Weight),
-			zap.String("price", order.Price.ToPriceString()),
-			zap.Float64("predictedPrice", weightedOrder.PredictedPrice),
-		)
-
-		switch knapsack.Put(order) {
-		case nil:
-		case errExhausted:
-			exhaustedCounter += 1
-			continue
-		default:
-			return fmt.Errorf("failed to consume order: %v", err)
-		}
-	}
-
-	return nil
 }
 
 type FittingFunc struct {
@@ -611,6 +553,16 @@ func NewKnapsack(deviceManager *DeviceManager) *Knapsack {
 	}
 }
 
+func (m *Knapsack) Clone() *Knapsack {
+	plans := make([]*sonm.AskPlan, len(m.plans))
+	copy(plans, m.plans)
+
+	return &Knapsack{
+		manager: m.manager.Clone(),
+		plans:   plans,
+	}
+}
+
 func (m *Knapsack) Put(order *sonm.Order) error {
 	resources, err := m.manager.Consume(*order.GetBenchmarks(), *order.GetNetflags())
 	if err != nil {
@@ -628,8 +580,13 @@ func (m *Knapsack) Put(order *sonm.Order) error {
 
 	return nil
 }
+
 func (m *Knapsack) Price() *sonm.Price {
 	return sonm.SumPrice(m.plans)
+}
+
+func (m *Knapsack) PPSf64() float64 {
+	return float64(m.Price().GetPerSecond().Unwrap().Uint64()) * 1e-18
 }
 
 func (m *Knapsack) Plans() []*sonm.AskPlan {
