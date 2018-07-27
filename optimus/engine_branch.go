bf0f80113336b428254fb5eac7b0abd1675110c0
Evgeny Safronov <division494@gmail.com>
2018-07-27 14:53:20 +0300

    feat: branch bound model
    This adds a new model - Branch Bound Tree, which finds a global optimum solution for knapsack problem.
    However, its complexity is still exponential, so it must be activated only when the number of matching orders is relatively low.

---
+++ optimus/engine_branch.go
@@ -0,0 +1,120 @@
+package optimus
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+)
+
+type ordersPool map[string]*MarketOrder
+
+func (m ordersPool) Clone() ordersPool {
+	pool := map[string]*MarketOrder{}
+	for _, order := range m {
+		id := order.GetOrder().GetId().Unwrap().String()
+		pool[id] = order
+	}
+
+	return pool
+}
+
+type node struct {
+	Knapsack   *Knapsack
+	OrdersPool map[string]*MarketOrder
+
+	depth    int
+	children []*node
+	log      *zap.SugaredLogger
+}
+
+func newNode(knapsack *Knapsack, ordersPool ordersPool, depth int, log *zap.SugaredLogger) *node {
+	children := make([]*node, 0, len(ordersPool))
+
+	for id, order := range ordersPool {
+		knapsack := knapsack.Clone()
+		if err := knapsack.Put(order.GetOrder()); err != nil {
+			continue
+		}
+
+		pool := ordersPool.Clone()
+		delete(pool, id)
+
+		if node := newNode(knapsack, pool, depth+1, log); node != nil {
+			children = append(children, node)
+		}
+	}
+
+	if len(children) == 0 {
+		log.Debugf("found leaf node %d", depth)
+	}
+
+	m := &node{
+		Knapsack:   knapsack,
+		OrdersPool: ordersPool,
+
+		depth:    depth,
+		children: children,
+		log:      log,
+	}
+
+	return m
+}
+
+func (m *node) FindOptimum() *Knapsack {
+	leafNodes := m.appendLeaf(make([]*node, 0, len(m.OrdersPool)*100))
+
+	var winnerNode *node
+	var winnerPrice = 0.0
+
+	for _, node := range leafNodes {
+		price := node.Knapsack.PPSf64()
+		if price > winnerPrice {
+			winnerNode = node
+			winnerPrice = price
+		}
+	}
+
+	return winnerNode.Knapsack
+}
+
+func (m *node) appendLeaf(nodes []*node) []*node {
+	if len(m.children) == 0 {
+		return append(nodes, m)
+	}
+
+	for _, child := range m.children {
+		nodes = child.appendLeaf(nodes)
+	}
+
+	return nodes
+}
+
+type BranchBoundModel struct {
+	Log *zap.SugaredLogger
+}
+
+func (m *BranchBoundModel) Optimize(knapsack *Knapsack, orders []*MarketOrder) error {
+	ordersPool := map[string]*MarketOrder{}
+	for _, order := range orders {
+		id := order.GetOrder().GetId().Unwrap().String()
+		ordersPool[id] = order
+	}
+
+	root := newNode(knapsack, ordersPool, 0, m.Log)
+	if root == nil {
+		return fmt.Errorf("failed to construct decision tree")
+	}
+
+	m.Log.Infof("successfully build branch bound model")
+
+	winner := root.FindOptimum()
+	if winner == nil {
+		return fmt.Errorf("failed to found optimum branch")
+	}
+
+	m.Log.Infof("successfully found optimum branch")
+
+	*knapsack = *winner
+
+	return nil
+}
