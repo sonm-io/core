56fea0ba7e6a8ade043c1da451f76b01d6d29684
Fedor Sakharov <fedor.sakharov@gmail.com>
2018-08-16 15:55:51 +0300

    fix: adds registry address to pandora

--- cmd/pandora/gun_marketplace.go
+++ cmd/pandora/gun_marketplace.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/ecdsa"
 
+	"github.com/sonm-io/core/util"
 	"github.com/sonm-io/core/blockchain"
 	"go.uber.org/zap"
 )
@@ -22,8 +23,14 @@ type marketplaceExt struct {
 func NewMarketplaceGun(cfg MarketplaceExtConfig) (Gun, error) {
 	privateKey := PrivateKey(cfg.Ethereum)
 
+	registryAddr, err := util.HexToAddress(cfg.Ethereum.Registry)
+	if err != nil {
+		return nil, err
+	}
+
 	market, err := blockchain.NewAPI(context.Background(),
-		blockchain.WithSidechainEndpoint(cfg.Ethereum.Endpoint))
+		blockchain.WithSidechainEndpoint(cfg.Ethereum.Endpoint),
+		blockchain.WithContractRegistry(registryAddr))
 	if err != nil {
 		return nil, err
 	}
