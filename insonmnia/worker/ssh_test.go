sonm-io/core

commit b79a42ea02dc18be751d268cd73f60d8a17e28c4

refactor: simplify task state storing and restoring (#1281)

insonmnia/worker/ssh.go
@@ -33,6 +33,23 @@ type SSHConfig struct {
 	Identity       sonm.IdentityLevel `yaml:"identity" default:"identified"`
 }
 
+type PublicKey struct {
+	ssh.PublicKey
+}
+
+func (m *PublicKey) UnmarshalText(data []byte) error {
+	pkey, _, _, _, err := ssh.ParseAuthorizedKey(data)
+	if err != nil {
+		return err
+	}
+	m.PublicKey = pkey
+	return nil
+}
+
+func (m PublicKey) MarshalText() ([]byte, error) {
+	return ssh.MarshalAuthorizedKey(m), nil
+}
+
 type SSH interface {
 	Run(ctx context.Context) error
 	Close() error

insonmnia/worker/ssh_test.go
@@ -1,0 +1,24 @@
+package worker
+
+import (
+	"testing"
+
+	"github.com/gliderlabs/ssh"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSSHKeyParse(t *testing.T) {
+	raw := []byte("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCh+u6UN26+nIc42aRhnuDeralPivXZDi3ETSugsNlOfMww5YdqSJc9otSGooPRbXhOVguoEZfBvLNNd4xTkYtaCsWmFGbq3JXCjtH22V3VeqDc1zd3iJGtQU2BInC0HHvR4M5U4ayN4Ur3bEwgBViv7J+2lABmOArVwOlxacI/m2FtmUPrXKLh98eZgvAxd7DLwTjL8DKLJVqk2hqPRbqvX+CVHVZ4EeS63k0ji2mHDDlZrCsm2n6CnOau4sIND4Xiibdtt6dHnXKXxyC1SLQlH1W+6fxdiQSWXK4/Q4ryA0L/t89CoSp+/uRy4xnP3z5ntI7vE+I3Y1kFeTpOy1v9")
+
+	pkey := PublicKey{}
+	err := pkey.UnmarshalText(raw)
+	require.NoError(t, err)
+
+	b, err := pkey.MarshalText()
+	require.NoError(t, err)
+
+	parsed, _, _, _, err := ssh.ParseAuthorizedKey(b)
+	require.NoError(t, err)
+	assert.True(t, ssh.KeysEqual(pkey, parsed))
+}

insonmnia/worker/container.go
@@ -31,7 +31,7 @@ type containerDescriptor struct {
 }
 
 func attachContainer(ctx context.Context, dockerClient *client.Client, ID string, d Description, tuners *plugin.Repository) (*containerDescriptor, error) {
-	log.S(ctx).Infof("attaching to container with ID: %s reference: %s", ID, d.Reference.String())
+	log.S(ctx).Infof("attaching to container with ID: %s reference: %s", ID, d.Reference.Reference().String())
 
 	cont := containerDescriptor{
 		client:      dockerClient,
@@ -50,7 +50,7 @@ func attachContainer(ctx context.Context, dockerClient *client.Client, ID string
 }
 
 func newContainer(ctx context.Context, dockerClient *client.Client, d Description, tuners *plugin.Repository) (*containerDescriptor, error) {
-	log.S(ctx).Infof("start container with application, reference %s", d.Reference.String())
+	log.S(ctx).Infof("start container with application, reference %s", d.Reference.Reference().String())
 
 	cont := containerDescriptor{
 		client:      dockerClient,
@@ -73,7 +73,7 @@ func newContainer(ctx context.Context, dockerClient *client.Client, d Descriptio
 
 		ExposedPorts: exposedPorts,
 
-		Image: d.Reference.String(),
+		Image: d.Reference.Reference().String(),
 		// TODO: set actual name
 		Labels:  map[string]string{overseerTag: "", dealIDTag: d.DealId},
 		Env:     d.FormatEnv(),
@@ -97,9 +97,9 @@ func newContainer(ctx context.Context, dockerClient *client.Client, d Descriptio
 	networkingConfig := network.NetworkingConfig{
 		EndpointsConfig: map[string]*network.EndpointSettings{},
 	}
-	if d.network != nil {
-		networkingConfig.EndpointsConfig[d.network.Name] = &network.EndpointSettings{
-			NetworkID: d.network.ID,
+	if d.NetworkOptions != nil {
+		networkingConfig.EndpointsConfig[d.NetworkOptions.Name] = &network.EndpointSettings{
+			NetworkID: d.NetworkOptions.ID,
 		}
 	}
 
@@ -245,11 +245,12 @@ func (c *containerDescriptor) upload(ctx context.Context) error {
 	}
 	tag := fmt.Sprintf("%s_%s", c.description.DealId, c.description.TaskId)
 
-	ref := c.description.Reference
+	ref := c.description.Reference.Reference()
 	if _, ok := ref.(reference.Named); !ok {
 		return errors.New("can not upload image without name")
 	}
-	newImg, err := reference.WithTag(reference.TrimNamed(c.description.Reference.(reference.Named)), tag)
+
+	newImg, err := reference.WithTag(reference.TrimNamed(c.description.Reference.Reference().(reference.Named)), tag)
 	if err != nil {
 		c.log.Errorf("failed to add tag: %s", err)
 		return err

insonmnia/worker/overseer.go
@@ -35,7 +35,7 @@ const dieEvent = "die"
 // Description for a target application.
 type Description struct {
 	pb.Container
-	Reference    reference.Reference
+	Reference    reference.Field
 	Auth         string
 	Resources    *pb.AskPlanResources
 	CGroupParent string
@@ -48,41 +48,12 @@ type Description struct {
 
 	mounts []volume.Mount
 
-	network  *network.Network
-	networks []*structs.NetworkSpec
+	NetworkOptions *network.Network
+	NetworkSpecs   []*structs.NetworkSpec
 }
 
 func (d *Description) ID() string {
 	return d.TaskId
-}
-
-type descriptionAlias Description
-
-type descriptionMarshaller struct {
-	*descriptionAlias
-	Networks []*structs.NetworkSpec
-	RefField reference.Field `json:"Reference"`
-}
-
-func (d Description) MarshalJSON() ([]byte, error) {
-	b, err := json.Marshal(&descriptionMarshaller{
-		descriptionAlias: (*descriptionAlias)(&d),
-		Networks:         d.networks,
-		RefField:         reference.AsField(d.Reference),
-	})
-
-	return b, err
-}
-
-func (d *Description) UnmarshalJSON(data []byte) error {
-	aux := &descriptionMarshaller{descriptionAlias: (*descriptionAlias)(d)}
-	if err := json.Unmarshal(data, &aux); err != nil {
-		return err
-	}
-
-	d.Reference = aux.RefField.Reference()
-	d.networks = aux.Networks
-	return nil
 }
 
 func (d *Description) Volumes() map[string]*pb.Volume {
@@ -94,11 +65,11 @@ func (d *Description) Mounts(source string) []volume.Mount {
 }
 
 func (d *Description) Network() (string, string) {
-	if d.network == nil {
+	if d.NetworkOptions == nil {
 		return "", ""
 	}
 
-	return d.network.Name, d.network.ID
+	return d.NetworkOptions.Name, d.NetworkOptions.ID
 }
 
 func (d *Description) DealID() string {
@@ -114,7 +85,7 @@ func (d *Description) GpuDeviceIDs() []gpu.GPUID {
 }
 
 func (d *Description) Networks() []*structs.NetworkSpec {
-	return d.networks
+	return d.NetworkSpecs
 }
 
 func (d *Description) FormatEnv() []string {
@@ -137,7 +108,7 @@ type ContainerInfo struct {
 	ImageName    string
 	StartAt      time.Time
 	Ports        nat.PortMap
-	PublicKey    ssh.PublicKey
+	PublicKey    PublicKey
 	Cgroup       string
 	CgroupParent string
 	NetworkIDs   []string
@@ -493,10 +464,10 @@ func (o *overseer) Spool(ctx context.Context, d Description) error {
 	if err != nil {
 		return err
 	}
-	refStr := d.Reference.String()
+	refStr := d.Reference.Reference().String()
 	for _, summary := range summaries {
 		if summary.ID == refStr {
-			log.S(ctx).Infof("application image %s is already present", d.Reference.String())
+			log.S(ctx).Infof("application image %s is already present", d.Reference.Reference().String())
 			return nil
 		}
 	}

insonmnia/worker/overseer_test.go
@@ -4,6 +4,7 @@ import (
 	"archive/tar"
 	"bytes"
 	"context"
+	"encoding/json"
 	"io"
 	"sync"
 	"testing"
@@ -13,7 +14,11 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
+	"github.com/gliderlabs/ssh"
+	"github.com/sonm-io/core/insonmnia/structs"
+	"github.com/sonm-io/core/insonmnia/worker/network"
 	"github.com/sonm-io/core/insonmnia/worker/plugin"
+	"github.com/sonm-io/core/proto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -26,11 +31,11 @@ func TestOvsSpool(t *testing.T) {
 
 	ref, err := reference.ParseNormalizedNamed("docker.io/alpine")
 	require.NoError(t, err, "failed to create Overseer")
-	err = ovs.Spool(ctx, Description{Reference: ref})
+	err = ovs.Spool(ctx, Description{Reference: reference.AsField(ref)})
 	require.NoError(t, err, "failed to pull an image")
 
 	ref, err = reference.ParseNormalizedNamed("docker2.io/alpine")
-	err = ovs.Spool(ctx, Description{Reference: ref})
+	err = ovs.Spool(ctx, Description{Reference: reference.AsField(ref)})
 	require.NotNil(t, err)
 }
 
@@ -111,7 +116,7 @@ func TestOvsSpawn(t *testing.T) {
 	require.NoError(t, err)
 	ref, err := reference.ParseNormalizedNamed("worker")
 	require.NoError(t, err)
-	ch, info, err := ovs.Start(ctx, Description{Reference: ref})
+	ch, info, err := ovs.Start(ctx, Description{Reference: reference.AsField(ref)})
 	require.NoError(t, err)
 	cjson, err := cl.ContainerInspect(ctx, info.ID)
 	require.NoError(t, err)
@@ -152,7 +157,7 @@ func TestOvsAttach(t *testing.T) {
 	require.NoError(t, err)
 	ref, err := reference.ParseNormalizedNamed("worker")
 	require.NoError(t, err)
-	_, info, err := ovs.Start(ctx, Description{Reference: ref})
+	_, info, err := ovs.Start(ctx, Description{Reference: reference.AsField(ref)})
 	require.NoError(t, err)
 	cjson, err := cl.ContainerInspect(ctx, info.ID)
 	require.NoError(t, err)
@@ -165,7 +170,7 @@ func TestOvsAttach(t *testing.T) {
 
 	ovs2, err := NewOverseer(ctx, plugin.EmptyRepository())
 	require.NoError(t, err)
-	descr := Description{Reference: ref}
+	descr := Description{Reference: reference.AsField(ref)}
 	ch, err := ovs2.Attach(ctx, info.ID, descr)
 	t.Logf("attached to container %s", info.ID)
 	require.NoError(t, err)
@@ -206,4 +211,83 @@ func TestExpose(t *testing.T) {
 		"10053/tcp": {{HostIP: "8.8.8.8", HostPort: "53"}},
 		"22/tcp":    {{HostIP: "", HostPort: ""}},
 	}, portBinding)
+}
+
+func TestMarshalDescription(t *testing.T) {
+	ref, err := reference.ParseNamed("docker.io/sonm-io/tests:latest")
+	require.NoError(t, err)
+
+	d := Description{
+		Reference: reference.AsField(ref),
+		NetworkOptions: &network.Network{
+			ID:               "1234",
+			Name:             "net_name",
+			Alias:            "net_alias",
+			RateLimitEgress:  100,
+			RateLimitIngress: 200,
+		},
+		NetworkSpecs: []*structs.NetworkSpec{
+			{
+				NetworkSpec: &sonm.NetworkSpec{
+					Type:    "type1",
+					Options: map[string]string{"opt1": "val1"},
+					Subnet:  "/12",
+					Addr:    "1.1.1.1",
+				},
+				NetID: "1111",
+			},
+			{
+				NetworkSpec: &sonm.NetworkSpec{
+					Type:    "type2",
+					Options: map[string]string{"opt2": "val2"},
+					Subnet:  "/28",
+					Addr:    "2.2.2.2",
+				},
+				NetID: "2222",
+			},
+		},
+	}
+
+	data, err := json.Marshal(&d)
+	require.NoError(t, err)
+
+	n := Description{}
+	err = json.Unmarshal(data, &n)
+	require.NoError(t, err)
+
+	assert.NotEmpty(t, n.Reference.Reference().String())
+	assert.Equal(t, d.Reference.Reference().String(), n.Reference.Reference().String())
+	assert.Equal(t, ref.String(), n.Reference.Reference().String())
+	assert.Equal(t, len(d.NetworkSpecs), len(n.NetworkSpecs))
+
+	assert.Equal(t, d.NetworkSpecs[0].Type, n.NetworkSpecs[0].Type)
+	assert.Equal(t, d.NetworkSpecs[0].Options, n.NetworkSpecs[0].Options)
+	assert.Equal(t, d.NetworkSpecs[0].Subnet, n.NetworkSpecs[0].Subnet)
+	assert.Equal(t, d.NetworkSpecs[0].Addr, n.NetworkSpecs[0].Addr)
+
+	assert.Equal(t, d.NetworkOptions.ID, n.NetworkOptions.ID)
+	assert.Equal(t, d.NetworkOptions.Name, n.NetworkOptions.Name)
+	assert.Equal(t, d.NetworkOptions.Alias, n.NetworkOptions.Alias)
+	assert.Equal(t, d.NetworkOptions.RateLimitEgress, n.NetworkOptions.RateLimitEgress)
+	assert.Equal(t, d.NetworkOptions.RateLimitIngress, n.NetworkOptions.RateLimitIngress)
+}
+
+func TestMarshalContainerInfo(t *testing.T) {
+	keyRaw := []byte("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCh+u6UN26+nIc42aRhnuDeralPivXZDi3ETSugsNlOfMww5YdqSJc9otSGooPRbXhOVguoEZfBvLNNd4xTkYtaCsWmFGbq3JXCjtH22V3VeqDc1zd3iJGtQU2BInC0HHvR4M5U4ayN4Ur3bEwgBViv7J+2lABmOArVwOlxacI/m2FtmUPrXKLh98eZgvAxd7DLwTjL8DKLJVqk2hqPRbqvX+CVHVZ4EeS63k0ji2mHDDlZrCsm2n6CnOau4sIND4Xiibdtt6dHnXKXxyC1SLQlH1W+6fxdiQSWXK4/Q4ryA0L/t89CoSp+/uRy4xnP3z5ntI7vE+I3Y1kFeTpOy1v9 alex@Dikobrazzers.local")
+	pkey := PublicKey{}
+	err := pkey.UnmarshalText(keyRaw)
+	require.NoError(t, err)
+
+	c := ContainerInfo{
+		PublicKey: pkey,
+	}
+
+	data, err := json.Marshal(c)
+	require.NoError(t, err)
+
+	n := ContainerInfo{}
+	err = json.Unmarshal(data, &n)
+	require.NoError(t, err)
+
+	assert.True(t, ssh.KeysEqual(c.PublicKey, n.PublicKey))
 }

insonmnia/worker/server.go
@@ -607,7 +607,7 @@ func (m *Worker) taskAllowed(ctx context.Context, request *pb.StartTaskRequest) 
 }
 
 func (m *Worker) StartTask(ctx context.Context, request *pb.StartTaskRequest) (*pb.StartTaskReply, error) {
-	allowed, reference, err := m.taskAllowed(ctx, request)
+	allowed, ref, err := m.taskAllowed(ctx, request)
 	if err != nil {
 		return nil, err
 	}
@@ -630,7 +630,12 @@ func (m *Worker) StartTask(ctx context.Context, request *pb.StartTaskRequest) (*
 	}
 
 	spec := request.GetSpec()
-	publicKey, err := parsePublicKey(spec.GetContainer().GetSshKey())
+
+	publicKey := PublicKey{}
+	err = publicKey.UnmarshalText([]byte(spec.GetContainer().GetSshKey()))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse SSH public key: %v", err)
+	}
 
 	network, err := m.salesman.Network(ask.ID)
 	if err != nil {
@@ -694,17 +699,17 @@ func (m *Worker) StartTask(ctx context.Context, request *pb.StartTaskRequest) (*
 	}
 
 	var d = Description{
-		Container:    *request.Spec.Container,
-		Reference:    reference,
-		Auth:         spec.Registry.Auth(),
-		CGroupParent: cgroup.Suffix(),
-		Resources:    spec.Resources,
-		DealId:       request.GetDealID().Unwrap().String(),
-		TaskId:       taskID,
-		GPUDevices:   gpuids,
-		mounts:       mounts,
-		network:      network,
-		networks:     networks,
+		Container:      *request.Spec.Container,
+		Reference:      reference.AsField(ref),
+		Auth:           spec.Registry.Auth(),
+		CGroupParent:   cgroup.Suffix(),
+		Resources:      spec.Resources,
+		DealId:         request.GetDealID().Unwrap().String(),
+		TaskId:         taskID,
+		GPUDevices:     gpuids,
+		mounts:         mounts,
+		NetworkOptions: network,
+		NetworkSpecs:   networks,
 	}
 
 	// TODO: Detect whether it's the first time allocation. If so - release resources on error.
@@ -725,9 +730,10 @@ func (m *Worker) StartTask(ctx context.Context, request *pb.StartTaskRequest) (*
 		m.setStatus(&pb.TaskStatusReply{Status: pb.TaskStatusReply_BROKEN}, taskID)
 		return nil, status.Errorf(codes.Internal, "failed to Spawn %v", err)
 	}
+
 	containerInfo.PublicKey = publicKey
 	containerInfo.StartAt = time.Now()
-	containerInfo.ImageName = reference.String()
+	containerInfo.ImageName = ref.String()
 	containerInfo.DealID = dealID.Unwrap().String()
 	containerInfo.Tag = request.GetSpec().GetTag()
 	containerInfo.TaskId = taskID
@@ -983,7 +989,6 @@ func (m *Worker) setupRunningContainers() error {
 			}
 
 			contJson, err := dockerClient.ContainerInspect(m.ctx, container.ID)
-
 			if err != nil {
 				log.S(m.ctx).Error("failed to inspect container", zap.String("id", container.ID), zap.Error(err))
 				return err
@@ -1318,12 +1323,12 @@ func parseBenchmarkResult(data []byte) (map[string]*bm.ResultJSON, error) {
 }
 
 func getDescriptionForBenchmark(b *pb.Benchmark) (Description, error) {
-	reference, err := reference.ParseNormalizedNamed(b.GetImage())
+	ref, err := reference.ParseNormalizedNamed(b.GetImage())
 	if err != nil {
 		return Description{}, err
 	}
 	return Description{
-		Reference: reference,
+		Reference: reference.AsField(ref),
 		Container: pb.Container{Env: map[string]string{
 			bm.BenchIDEnvParamName: fmt.Sprintf("%d", b.GetID()),
 		}},

commit 227139314eed60bf1ec16d638d57a4bec9775274

fix: proper (un)marshaling for empty SSH keys
This commit fixes couple bugs introduced in #1281.
1. Failed to start a task without providing ssh pubkey.
2. Marshalling an empty pubkey crashes worker.

insonmnia/worker/ssh.go
@@ -38,16 +38,23 @@ type PublicKey struct {
 }
 
 func (m *PublicKey) UnmarshalText(data []byte) error {
-	pkey, _, _, _, err := ssh.ParseAuthorizedKey(data)
-	if err != nil {
-		return err
+	if len(data) > 0 {
+		pkey, _, _, _, err := ssh.ParseAuthorizedKey(data)
+		if err != nil {
+			return err
+		}
+		m.PublicKey = pkey
 	}
-	m.PublicKey = pkey
+
 	return nil
 }
 
 func (m PublicKey) MarshalText() ([]byte, error) {
-	return ssh.MarshalAuthorizedKey(m), nil
+	if m.PublicKey != nil {
+		return ssh.MarshalAuthorizedKey(m), nil
+	}
+
+	return []byte{}, nil
 }
 
 type SSH interface {
@@ -235,19 +242,6 @@ func (m *sshServer) Close() error {
 	m.log.Info("closing ssh server")
 
 	return m.server.Close()
-}
-
-func parsePublicKey(key string) (ssh.PublicKey, error) {
-	var publicKey ssh.PublicKey
-	if len(key) != 0 {
-		k, _, _, _, err := ssh.ParseAuthorizedKey([]byte(key))
-		if err != nil {
-			return nil, err
-		}
-		publicKey = k
-	}
-
-	return publicKey, nil
 }
 
 func capitalize(s string) string {

insonmnia/worker/ssh_test.go
@@ -1,6 +1,7 @@
 package worker
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/gliderlabs/ssh"
@@ -21,4 +22,15 @@ func TestSSHKeyParse(t *testing.T) {
 	parsed, _, _, _, err := ssh.ParseAuthorizedKey(b)
 	require.NoError(t, err)
 	assert.True(t, ssh.KeysEqual(pkey, parsed))
+}
+
+func TestMarshalEmpty(t *testing.T) {
+
+	var one PublicKey
+	data, err := json.Marshal(one)
+	require.NoError(t, err)
+
+	two := PublicKey{}
+	err = json.Unmarshal(data, &two)
+	require.NoError(t, err)
 }