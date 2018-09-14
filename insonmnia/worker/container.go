3e1193dbf1602e66182bb05efb799f1b5c149238
Anton Matveenko <antmat@me.com>
2018-09-14 18:07:09 +0300

    refactor: handy reference wrapper

--- insonmnia/worker/container.go
+++ insonmnia/worker/container.go
@@ -3,11 +3,9 @@ package worker
 import (
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
 	"io"
 
-	"github.com/docker/distribution/reference"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
@@ -33,7 +31,7 @@ type containerDescriptor struct {
 }
 
 func attachContainer(ctx context.Context, dockerClient *client.Client, ID string, d Description, tuners *plugin.Repository) (*containerDescriptor, error) {
-	log.S(ctx).Infof("attaching to container with ID: %s reference: %s", ID, d.Reference.Reference().String())
+	log.S(ctx).Infof("attaching to container with ID: %s reference: %s", ID, d.Reference.String())
 
 	cont := containerDescriptor{
 		client:      dockerClient,
@@ -52,7 +50,7 @@ func attachContainer(ctx context.Context, dockerClient *client.Client, ID string
 }
 
 func newContainer(ctx context.Context, dockerClient *client.Client, d Description, tuners *plugin.Repository) (*containerDescriptor, error) {
-	log.S(ctx).Infof("start container with application, reference %s", d.Reference.Reference().String())
+	log.S(ctx).Infof("start container with application, reference %s", d.Reference.String())
 
 	cont := containerDescriptor{
 		client:      dockerClient,
@@ -75,7 +73,7 @@ func newContainer(ctx context.Context, dockerClient *client.Client, d Descriptio
 
 		ExposedPorts: exposedPorts,
 
-		Image: d.Reference.Reference().String(),
+		Image: d.Reference.String(),
 		// TODO: set actual name
 		Labels:  map[string]string{overseerTag: "", dealIDTag: d.DealId},
 		Env:     d.FormatEnv(),
@@ -248,12 +246,7 @@ func (c *containerDescriptor) upload(ctx context.Context) error {
 	}
 	tag := fmt.Sprintf("%s_%s", c.description.DealId, c.description.TaskId)
 
-	ref := c.description.Reference.Reference()
-	if _, ok := ref.(reference.Named); !ok {
-		return errors.New("can not upload image without name")
-	}
-
-	newImg, err := reference.WithTag(reference.TrimNamed(c.description.Reference.Reference().(reference.Named)), tag)
+	newImg, err := c.description.Reference.WithTag(tag)
 	if err != nil {
 		c.log.Errorf("failed to add tag: %s", err)
 		return err
---
+++ util/xdocker/reference.go
@@ -0,0 +1,95 @@
+package xdocker
+
+import (
+	"github.com/docker/distribution/reference"
+	"github.com/opencontainers/go-digest"
+	"github.com/pkg/errors"
+)
+
+type Reference struct {
+	reference.Reference
+}
+
+func NewReference(source string) (Reference, error) {
+	ref := Reference{}
+	if err := ref.Parse(source); err != nil {
+		return Reference{}, err
+	}
+	return ref, nil
+}
+
+func (m *Reference) Parse(source string) error {
+	ref, err := reference.ParseAnyReference(source)
+	if err != nil {
+		return err
+	}
+	m.Reference = ref
+	return nil
+}
+
+func (m *Reference) UnmarshalText(source []byte) error {
+	return m.Parse(string(source))
+}
+
+func (m *Reference) MarshalText() ([]byte, error) {
+	return []byte(m.String()), nil
+}
+
+func (m *Reference) Named() reference.Named {
+	if ref, ok := m.Reference.(reference.Named); ok {
+		return ref
+	}
+	return nil
+}
+
+func (m *Reference) WithTag(tag string) (Reference, error) {
+	named := m.Named()
+	if named == nil {
+		return Reference{}, errors.New("reference is not named and thus can not be tagged")
+	}
+	named = reference.TrimNamed(named)
+	ref, err := reference.WithTag(named, tag)
+	if err != nil {
+		return Reference{}, err
+	}
+	return Reference{ref}, nil
+}
+
+func (m *Reference) HasDigest() bool {
+	digested, ok := m.Reference.(reference.Digested)
+	return ok && len(digested.Digest()) > 0
+}
+
+func (m *Reference) Digest() digest.Digest {
+	digested, ok := m.Reference.(reference.Digested)
+	if !ok {
+		return ""
+	}
+	return digested.Digest()
+}
+
+func (m *Reference) WithDigest(digest digest.Digest) (Reference, error) {
+	named := m.Named()
+	if named == nil {
+		return Reference{}, errors.New("reference is not named and thus can not be digested")
+	}
+	named = reference.TrimNamed(named)
+	ref, err := reference.WithDigest(named, digest)
+	if err != nil {
+		return Reference{}, err
+	}
+	return Reference{ref}, nil
+}
+
+func (m *Reference) HasName() bool {
+	named, ok := m.Reference.(reference.Named)
+	return ok && len(named.Name()) > 0
+}
+
+func (m *Reference) Name() string {
+	named, ok := m.Reference.(reference.Named)
+	if !ok {
+		return ""
+	}
+	return named.Name()
+}
