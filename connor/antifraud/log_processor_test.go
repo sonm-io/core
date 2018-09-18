b36d82899d3328b879279a5a78bd72aebf481510
ALex Nikonov <alex@sshaman.ru>
2018-09-18 20:10:40 +0300

    fix: proper log processor for Connor (#1514)
    This commit fixes several issues with log processor:
    - Wrap calculated hashrate value with atomic to make it thread-safe.
    - Perform initializing tick for EWMA.
    - Add timestamps for stored logs.
    - Read logs with tail. It prevents us from reloading full task log when Connor restarts or when connection was lost and Connot should start read logs again.

--- connor/antifraud/log_processor_test.go
+++ connor/antifraud/log_processor_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/atomic"
 	"go.uber.org/zap"
 )
 
@@ -20,27 +21,27 @@ func mklog(n int) string {
 
 func TestClaymoreLogParser(t *testing.T) {
 	rd := strings.NewReader(`ETH - Total Speed: 100.000 Mh/s, Total Shares: 127, Rejected: 0, Time: 00:02`)
-	p := &commonLogProcessor{log: zap.NewNop()}
+	p := &commonLogProcessor{log: zap.NewNop(), hashrate: atomic.NewFloat64(0)}
 
 	claymoreLogParser(context.Background(), p, rd)
-	assert.Equal(t, float64(100e6), p.hashrate, "new value should be parsed and set")
+	assert.Equal(t, float64(100e6), p.hashrate.Load(), "new value should be parsed and set")
 }
 
 func TestClaymoreLogParser_InvalidLine(t *testing.T) {
 	rd := strings.NewReader(`Oops! Claymore failed`)
-	p := &commonLogProcessor{log: zap.NewNop(), hashrate: 100500}
+	p := &commonLogProcessor{log: zap.NewNop(), hashrate: atomic.NewFloat64(100500)}
 
 	claymoreLogParser(context.Background(), p, rd)
-	assert.Equal(t, float64(100500), p.hashrate, "previous value should be kept")
+	assert.Equal(t, float64(100500), p.hashrate.Load(), "previous value should be kept")
 }
 
 func TestClaymoreLogParser_ContextCancel(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	rd := strings.NewReader(mklog(1000))
-	p := &commonLogProcessor{log: zap.NewNop(), hashrate: 1.2345}
+	p := &commonLogProcessor{log: zap.NewNop(), hashrate: atomic.NewFloat64(1.2345)}
 	cancel()
 
 	claymoreLogParser(ctx, p, rd)
-	assert.Equal(t, float64(1.2345), p.hashrate, "new value should be parsed and set")
+	assert.Equal(t, float64(1.2345), p.hashrate.Load(), "new value should be parsed and set")
 }
