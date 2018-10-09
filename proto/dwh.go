e33b0d9f4f83ccd3b5976714fc7d0f0955c085e0
Evgeny Safronov <division494@gmail.com>
2018-10-09 11:08:57 +0300

    refactor: dead code elimination

--- proto/dwh.go
+++ proto/dwh.go
@@ -17,21 +17,6 @@ func (m *Benchmarks) ToArray() []uint64 {
 	return m.Values
 }
 
-func (m *Benchmarks) Contains(other *Benchmarks) bool {
-	return m.CPUSysbenchMulti() >= other.CPUSysbenchMulti() &&
-		m.CPUSysbenchOne() >= other.CPUSysbenchOne() &&
-		m.CPUCores() >= other.CPUCores() &&
-		m.RAMSize() >= other.RAMSize() &&
-		m.StorageSize() >= other.StorageSize() &&
-		m.NetTrafficIn() >= other.NetTrafficIn() &&
-		m.NetTrafficOut() >= other.NetTrafficOut() &&
-		m.GPUCount() >= other.GPUCount() &&
-		m.GPUMem() >= other.GPUMem() &&
-		m.GPUEthHashrate() >= other.GPUEthHashrate() &&
-		m.GPUCashHashrate() >= other.GPUCashHashrate() &&
-		m.GPURedshift() >= other.GPURedshift()
-}
-
 // GetAttributeName converts profile cert attr number to
 // human readable name.
 func (m *Certificate) GetAttributeName() string {
