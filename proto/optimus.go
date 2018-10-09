55c2921de90121d7b971de557cd30db9cf440ac7
Evgeny Safronov <division494@gmail.com>
2018-10-09 17:28:16 +0300

    feat: validate prediction request
    Changelog: exclude.

--- proto/optimus.go
+++ proto/optimus.go
@@ -1,5 +1,17 @@
 package sonm
 
+import (
+	"fmt"
+)
+
+func (m *PredictSupplierRequest) Validate() error {
+	if len(m.GetDevices().GetGPUs()) > 16 {
+		return fmt.Errorf("number of GPUs must be <= 16")
+	}
+
+	return nil
+}
+
 func (m *PredictSupplierRequest) Normalize() {
 	for _, dev := range m.GetDevices().GetGPUs() {
 		dev.GetDevice().FillHashID()
