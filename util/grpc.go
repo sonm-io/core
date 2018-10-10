2f4ce8b818e14a1192751f04efb739f66f3746bf
Evgeny Safronov <division494@gmail.com>
2018-10-10 20:05:39 +0300

    refactor: dead code (#1627)

--- util/grpc.go
+++ util/grpc.go
@@ -1,14 +1,5 @@
 package util
 
-import (
-	"strings"
-)
-
 const (
 	WorkerAddressHeader = "x-worker-eth-addr"
 )
-
-func ExtractMethod(fullMethod string) string {
-	parts := strings.Split(fullMethod, "/")
-	return parts[len(parts)-1]
-}
