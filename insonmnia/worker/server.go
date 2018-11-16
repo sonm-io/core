ee02474358d6d12f7006200cf1e902cb8eb300bd
Evgeny Safronov <division494@gmail.com>
2018-11-16 12:17:05 +0300

    fix: simplify
    Co-Authored-By: antmat <antmat@mail.ru>

--- insonmnia/worker/server.go
+++ insonmnia/worker/server.go
@@ -1015,7 +1015,7 @@ func (m *Worker) taskAllowed(ctx context.Context, request *sonm.StartTaskRequest
 	}
 	if level < m.cfg.Whitelist.PrivilegedIdentityLevel {
 		volumes := spec.GetContainer().GetVolumes()
-		mounts := request.GetSpec().GetContainer().GetMounts()
+		mounts := spec.GetContainer().GetMounts()
 		if (volumes != nil && len(volumes) > 0) || (mounts != nil && len(mounts) > 0) {
 			return false, ref, fmt.Errorf("mounting volumes is forbidden due to kyc level")
 		}
