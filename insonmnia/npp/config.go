56952116efb1cb3960f279c1c7e6bf9e325be22a
Evgeny Safronov <division494@gmail.com>
2018-12-05 15:36:28 +0300

    docs: tiny comment (#1774)

--- insonmnia/npp/config.go
+++ insonmnia/npp/config.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sonm-io/core/insonmnia/npp/rendezvous"
 )
 
+// Config represents an NPP (NAT punching protocol) module configuration.
 type Config struct {
 	Rendezvous         rendezvous.Config `yaml:"rendezvous"`
 	Relay              relay.Config      `yaml:"relay"`
