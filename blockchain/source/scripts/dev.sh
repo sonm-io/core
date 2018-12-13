1c7e8fc664e048c05bfdec226191a7e935e0b8a5
Anton Matveenko <antmat@me.com>
2018-12-13 17:25:17 +0300

    fix: ease of use

---
+++ blockchain/source/scripts/dev.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+trap cleanup EXIT
+
+pids=()
+
+blockTime=$1
+if [[ -n "$blockTime" ]]; then
+    blockTime="-b $blockTime"
+fi
+
+cleanup() {
+  echo "huy"
+  for pid in "${pids[@]}"
+  do
+    echo "killing $pid"
+    kill $pid
+  done
+  pids=()
+}
+
+rpc_running() {
+  nc -z localhost "$1"
+}
+
+run() {
+    port=$1;
+    chain=$2
+    mkdir -p ./$port.db
+    if rpc_running $port; then
+        echo "rpc is running on $port"
+    else
+        echo "running node_modules/.bin/ganache-cli $blockTime -d sonm --port=$port -i $port >> ./$port.log &"
+        node_modules/.bin/ganache-cli $blockTime -d sonm --port=$port -i $port >> ./$port.log &
+        pids+=($!)
+        echo "started ganache on $port (pid $!)"
+    fi
+
+}
+
+run 8545 main
+run 8525 side
+
+wait
