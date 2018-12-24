0fe93664c05236c6ae449aa6dc5e3cf6ed8a3bd7
Anton Matveenko <antmat@mail.ru>
2018-12-24 15:58:35 +0300

    chore: performed live migration of devices storage (#1814)

--- blockchain/source/migrations/3_deploy_devices_storage.js
+++ blockchain/source/migrations/3_deploy_devices_storage.js
@@ -6,12 +6,21 @@ const ContractRegistry = require('../migration_utils/address_hashmap');
 
 async function deploySidechain (deployer, network, accounts) {
     let registry = new ContractRegistry(AddressHashMap, network, Multisig);
+    console.log('registry created');
     await registry.init();
+    console.log('registry initialized');
     await deployer.deploy(DevicesStorage);
+    console.log('devices storage deployed');
     let ds = await DevicesStorage.deployed();
+    console.log('deployed object fetched');
     await registry.write('devicesStorageAddress', ds.address);
-    let ms = await registry.resolve(Multisig, 'migrationMultiSigAddress');
+    console.log('written to registry');
+    let ms = await registry.resolve(Multisig, 'migrationMultSigAddress');
+    console.log('ms resolved');
     await ds.transferOwnership(ms.address);
+    console.log('ownership transferred');
+
+    await registry.write('migrationMultiSigAddress', ms.address);
 }
 
 module.exports = function (deployer, network, accounts) {
