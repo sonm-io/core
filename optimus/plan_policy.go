174115303671733219dfb30e2ec67c701d22a3a3
Evgeny Safronov <division494@gmail.com>
2019-05-22 15:49:10 +0300

    fix: properly decode plan policy from config in Optimus

--- optimus/plan_policy.go
+++ optimus/plan_policy.go
@@ -1,5 +1,9 @@
 package optimus
 
+import (
+	"fmt"
+)
+
 const (
 	planPolicyPrecise = iota
 	planPolicyEntireMachine
@@ -15,4 +19,19 @@ func (m *planPolicy) IsPrecise() bool {
 
 func (m *planPolicy) IsEntireMachine() bool {
 	return m.Type == planPolicyEntireMachine
+}
+
+func (m *planPolicy) UnmarshalText(text []byte) error {
+	ty := string(text)
+
+	switch ty {
+	case "precise":
+		*m = planPolicy{Type: planPolicyPrecise}
+	case "entire_machine":
+		*m = planPolicy{Type: planPolicyEntireMachine}
+	default:
+		return fmt.Errorf("unknown plan policy: %s", ty)
+	}
+
+	return nil
 }
