05e8203d7177276b6c1ab15dda5357a1632c0d78
Evgeny Safronov <division494@gmail.com>
2019-08-15 17:38:09 +0300

    fix: allow to start QOS without secsh

--- cmd/qos/main.go
+++ cmd/qos/main.go
@@ -95,6 +95,10 @@ func run(app cmd.AppContext) error {
 		return listener.Close()
 	})
 	wg.Go(func() error {
+		if cfg.SecShell == nil {
+			return nil
+		}
+
 		server, err := secsh.NewRemotePTYServer(cfg.SecShell, log.Sugar().With(zap.String("scope", "secsh")))
 		if err != nil {
 			return fmt.Errorf("failed to create remote PTY server: %v", err)
